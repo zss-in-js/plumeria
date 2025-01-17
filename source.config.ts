c6d60943175639de310d7388c4ae162f700e90f4
S.Watanabe <ocelot2828@gmail.com>
2025-01-17 22:31:20 +0900

    feat: add code syntax light dark

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,20 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig } from "fumadocs-mdx/config";
 
 export const { docs, meta } = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
 });
 
-export default defineConfig();
+export default defineConfig({
+  lastModifiedTime: "git",
+  mdxOptions: async () => {
+    return {
+      rehypeCodeOptions: {
+        inline: "tailing-curly-colon",
+        themes: {
+          light: "catppuccin-latte",
+          dark: "andromeeda",
+        },
+      },
+    };
+  },
+});
