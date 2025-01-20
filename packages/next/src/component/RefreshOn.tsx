8f38ab7e6260c4263d8bc3f2163d8db4f4f8470a
S.Watanabe <ocelot2828@gmail.com>
2025-01-21 05:38:19 +0900

    feat: supports hot reloading

--- packages/next/src/component/RefreshOn.tsx
+++ packages/next/src/component/RefreshOn.tsx
@@ -1,41 +1,67 @@
 'use client';
-
-import { useEffect, useCallback } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export const RefreshOn = () => {
   const router = useRouter();
-  const pathname = usePathname();
-
-  const handleSave = useCallback(
-    (e: KeyboardEvent) => {
-      if ((e.ctrlKey || e.metaKey) && e.key === 's') {
-        router.refresh();
-      }
-    },
-    [router]
-  );
-
-  const handleClick = useCallback(
-    (e: MouseEvent) => {
-      const target = e.target as HTMLElement;
-      const targetAnchor = target.closest('a');
-      if (pathname && targetAnchor instanceof HTMLAnchorElement && targetAnchor.origin === window.location.origin) {
-        router.refresh();
-      }
-    },
-    [pathname, router]
-  );
 
   useEffect(() => {
+    let shouldObserve = true;
+    let timeoutClick: NodeJS.Timeout;
+    let timeoutObserve: NodeJS.Timeout;
+
+    const handleClick = (e: MouseEvent) => {
+      const targetAnchor = (e.target as HTMLElement).closest('a');
+      if (targetAnchor instanceof HTMLAnchorElement && targetAnchor.origin === window.location.origin && targetAnchor.pathname !== window.location.pathname) {
+        shouldObserve = false;
+        router.refresh();
+        clearTimeout(timeoutClick);
+        timeoutClick = setTimeout(() => {
+          shouldObserve = true;
+        }, 200);
+      }
+    };
+
+    let isRefreshing = false;
+    const observeStyleSheets = () => {
+      const styleObserver = new MutationObserver(mutations => {
+        if (!shouldObserve || isRefreshing) return;
+
+        for (const mutation of mutations) {
+          if (mutation.type === 'childList') {
+            const addedNodes = Array.from(mutation.addedNodes);
+            if (
+              addedNodes.some(
+                node =>
+                  node instanceof HTMLStyleElement || (node instanceof HTMLLinkElement && !(node.rel === 'preload' && node.getAttribute('as') === 'style'))
+              )
+            ) {
+              isRefreshing = true;
+              router.refresh();
+              clearTimeout(timeoutObserve);
+              timeoutObserve = setTimeout(() => {
+                isRefreshing = false;
+              }, 20);
+              break;
+            }
+          }
+        }
+      });
+
+      styleObserver.observe(document.head, { childList: true });
+      return styleObserver;
+    };
+
     document.addEventListener('click', handleClick);
-    window.addEventListener('keydown', handleSave);
+    const cssObserver = observeStyleSheets();
 
     return () => {
       document.removeEventListener('click', handleClick);
-      window.removeEventListener('keydown', handleSave);
+      cssObserver.disconnect();
+      clearTimeout(timeoutClick);
+      clearTimeout(timeoutObserve);
     };
-  }, [router, pathname]);
+  }, [router]);
 
   return null;
 };
