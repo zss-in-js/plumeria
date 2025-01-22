f8671bbfb14505d924e39743880e4c2bb29325b5
S.Watanabe <ocelot2828@gmail.com>
2025-01-22 15:46:31 +0900

    fix: eslint fix

--- examples/with-next-app-router/src/app/style.ts
+++ examples/with-next-app-router/src/app/style.ts
@@ -1,10 +1,10 @@
-import { css } from "@plumeria/core";
+import { css } from '@plumeria/core';
 
 export const style = css.create({
   color: {
-    background: "linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
     fontSize: 40,
+    WebkitTextFillColor: 'transparent',
+    background: 'linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)',
+    WebkitBackgroundClip: 'text',
   },
 });
