cb1d66781ae5973240f8ddc075e5c71f51c4b532
S.Watanabe <ocelot2828@gmail.com>
2025-01-25 10:54:46 +0900

    feat: update flatConfig rules ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,21 @@ import objectCss from 'eslint-plugin-object-css';
 const eslintConfig = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  objectCss.flatConfigs.recommended,
+  {
+    ignores: ['**/dist/**', '**/.next/**', '**/*.js', '**/*css.mjs'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'object-css': objectCss,
+    },
+    rules: {
+      'object-css/valid-value': 'warn',
+      'object-css/recess-order': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-extraneous-class': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
   }
 );
 
