629654e0f6fc08a96c517a620bd78c335ccdd243
S.Watanabe <ocelot2828@gmail.com>
2025-02-05 15:09:04 +0900

    feat: add not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+'use client';
+import Link from 'next/link';
+import { JSX } from 'react';
+import { css } from '@plumeria/core';
+
+const styles = css.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    flexDirection: 'column',
+  },
+});
+
+const NotFound = (): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <h1>ClientSide Error status code 404 Request page is not found:</h1>
+      <Link href="/">
+        <u>Return to Top page</u>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
