a3e946535f518a37696c6b8177dbfecee1d3a597
S.Watanabe <ocelot2828@gmail.com>
2025-02-07 23:23:13 +0900

    feat: add meta title

--- website/app/(home)/layout.tsx
+++ website/app/(home)/layout.tsx
@@ -1,7 +1,12 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
-import './style.ts';
+import './global.ts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Plumeria - Zero-runtime CSS in JS library',
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
