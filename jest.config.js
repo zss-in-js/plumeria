a07db68b0765ff32488a318e684d61bb8a08d2a3
S.Watanabe <ocelot2828@gmail.com>
2025-02-07 10:25:06 +0900

    fix: folder name __tests__

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.ts?$': '@swc/jest',
   },
-  roots: ['<rootDir>/packages/core/__test__'],
+  roots: ['<rootDir>/packages/core/__tests__'],
   testEnvironment: 'jsdom',
   coverageReporters: ['text', 'html'],
   reporters: [['github-actions', { silent: false }], 'summary'],
