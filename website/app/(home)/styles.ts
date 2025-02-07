524df3d3e846957b125453cd8e1054836b130ec8
S.Watanabe <ocelot2828@gmail.com>
2025-02-07 23:26:58 +0900

    feat(styles): add with mdx

---
+++ website/app/(home)/styles.ts
@@ -0,0 +1,39 @@
+import { css } from '@plumeria/core';
+
+export const styles = css.create({
+  code_div: {
+    position: 'absolute',
+    top: 275,
+    right: 0,
+    left: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    [css.media.max('width: 804px')]: {
+      position: 'static',
+      top: 640,
+      right: 'auto',
+      left: 'auto',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      margin: '40px auto',
+      '& pre': {
+        width: 'auto',
+        maxWidth: '100%',
+        transform: 'none',
+      },
+    },
+    '& pre': {
+      zIndex: 2,
+      width: '355.33px',
+      height: '242px',
+      padding: '22px 44px',
+      fontSize: '12px',
+      borderRadius: '4px',
+      transform: 'translateX(200px)',
+    },
+  },
+});
