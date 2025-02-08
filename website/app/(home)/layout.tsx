f4e00ce26776ee13f1f7d2c3b0d653350ab7a22c
S.Watanabe <ocelot2828@gmail.com>
2025-02-08 15:30:50 +0900

    fix: folder tidy up

--- website/app/(home)/layout.tsx
+++ website/app/(home)/layout.tsx
@@ -1,8 +1,8 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
-import './global.ts';
 import { Metadata } from 'next';
+import './styles/global.ts';
 
 export const metadata: Metadata = {
   title: 'Plumeria - Zero-runtime CSS in JS library',
