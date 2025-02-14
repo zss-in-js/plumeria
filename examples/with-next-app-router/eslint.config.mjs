890c2feb32113bf644582112e7fd42da48b84599
S.Watanabe <ocelot2828@gmail.com>
2025-02-14 16:17:18 +0900

    feat: write __direname for node.js 21.1.0 or higher

--- examples/with-next-app-router/eslint.config.mjs
+++ examples/with-next-app-router/eslint.config.mjs
@@ -1,10 +1,7 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import objectCss from 'eslint-plugin-object-css';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = import.meta.dirname;
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
