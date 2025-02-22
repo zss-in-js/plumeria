f47c319850611831e8ba8f203d17646081e5fb9e
S.Watanabe <ocelot2828@gmail.com>
2025-02-22 16:24:17 +0900

    feat: editOnGithub

--- website/app/docs/[[...slug]]/page.tsx
+++ website/app/docs/[[...slug]]/page.tsx
@@ -1,16 +1,9 @@
 import { source } from '@/lib/source';
-import {
-  DocsPage,
-  DocsBody,
-  DocsDescription,
-  DocsTitle,
-} from 'fumadocs-ui/page';
+import { DocsPage, DocsBody, DocsDescription, DocsTitle } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 
-export default async function Page(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
+export default async function Page(props: { params: Promise<{ slug?: string[] }> }) {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
@@ -22,12 +15,12 @@ export default async function Page(props: {
       toc={page.data.toc}
       full={page.data.full}
       // lastUpdate={new Date(page.data.lastModified || '')}
-      // editOnGithub={{
-      //   owner: 'refirst11',
-      //   repo: 'plumeria-docs',
-      //   sha: 'main',
-      //   path: `content/docs/${page.file.path}`,
-      // }}
+      editOnGithub={{
+        owner: 'zss-in-js',
+        repo: 'plumeria',
+        sha: 'main',
+        path: `website/content/docs/${page.file.path}`,
+      }}
       tableOfContent={{
         style: 'clerk',
         single: false,
@@ -46,9 +39,7 @@ export async function generateStaticParams() {
   return source.generateParams();
 }
 
-export async function generateMetadata(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
+export async function generateMetadata(props: { params: Promise<{ slug?: string[] }> }) {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
