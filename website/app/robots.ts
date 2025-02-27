b8856c73d2f0b46ba9306c9d0ca1e25672271bec
S.Watanabe <ocelot2828@gmail.com>
2025-02-27 22:06:40 +0900

    fix: Environment Variables

--- website/app/robots.ts
+++ website/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: process.env.VERCEL_URL + '/sitemap.xml',
+    sitemap: process.env.PROD_URL + '/sitemap.xml',
   };
 }
