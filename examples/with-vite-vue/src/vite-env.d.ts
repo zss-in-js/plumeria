7dd1110eb322174ccd5a8357a99b7113caf05654
S.Watanabe <ocelot2828@gmail.com>
2025-02-28 16:19:53 +0900

    feat: add vue extention types

--- examples/with-vite-vue/src/vite-env.d.ts
+++ examples/with-vite-vue/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+// vue-shim declare types
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<object, object, any>;
+  export default component;
+}
