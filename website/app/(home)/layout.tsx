4127287eef14f5b7c07e42d047520911e85af5fa
S.Watanabe <ocelot2828@gmail.com>
2025-03-18 10:09:37 +0900

    feat: top Open Graph

--- website/app/(home)/layout.tsx
+++ website/app/(home)/layout.tsx
@@ -3,10 +3,11 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 import { Metadata } from 'next';
 import './styles/global.ts';
+import { metadataImage } from '@/lib/metadata';
 
-export const metadata: Metadata = {
+export const metadata: Metadata = metadataImage.withImage([], {
   title: 'Plumeria - Zero-runtime CSS in JS library',
-};
+});
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
