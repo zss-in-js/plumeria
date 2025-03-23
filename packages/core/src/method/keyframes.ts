94a3061ce29c915ca7156799e60ca0b6ee727c45
S.Watanabe <ocelot2828@gmail.com>
2025-03-23 17:18:37 +0900

    fix: import type

--- packages/core/src/method/keyframes.ts
+++ packages/core/src/method/keyframes.ts
@@ -1,4 +1,4 @@
-import { KeyframesDefinition, genBase36Hash } from 'zss-engine';
+import { type KeyframesDefinition, genBase36Hash } from 'zss-engine';
 import { global } from './global';
 
 export const keyframes = (object: KeyframesDefinition) => {
