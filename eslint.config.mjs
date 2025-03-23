6497bcb15e50db7e976392c57d018aa07c9bac04
S.Watanabe <ocelot2828@gmail.com>
2025-03-23 15:43:58 +0900

    feat: linter update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import objectCss from 'eslint-plugin-object-css';
+import zsslint from 'eslint-plugin-zss-lint';
 
 const eslintConfig = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
+  zsslint.flatConfigs.recommended,
   {
     ignores: [
       '**/dist/**',
@@ -16,12 +17,7 @@ const eslintConfig = tseslint.config(
   },
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: {
-      'object-css': objectCss,
-    },
     rules: {
-      'object-css/valid-value': 'warn',
-      'object-css/recess-order': 'warn',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-extraneous-class': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
