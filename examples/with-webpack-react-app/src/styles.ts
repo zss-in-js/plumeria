f48b09c666319ef6f8897b20793bacd12f2fbbb8
S.Watanabe <ocelot2828@gmail.com>
2025-03-26 17:18:16 +0900

    fix: removed old webpack react

--- examples/with-webpack-react-app/src/styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from '@plumeria/core';
-
-export const styles = css.create({
-  color: {
-    WebkitTextFillColor: 'transparent',
-    background: 'linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)',
-    WebkitBackgroundClip: 'text',
-  },
-});
