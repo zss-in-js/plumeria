61449f7ba3d95a74b4e5a4970a913b35b2eea6ae
S.Watanabe <ocelot2828@gmail.com>
2025-03-29 22:41:16 +0900

    feat(compiler): create type

---
+++ packages/compiler/d.ts
@@ -0,0 +1,7 @@
+declare module '@plumeria/core/dist/method/global-build-helper' {
+  export function buildGlobal(): Promise<void>;
+}
+
+declare module '@plumeria/core/dist/method/create-build-helper' {
+  export function buildCreate(): Promise<void>;
+}
