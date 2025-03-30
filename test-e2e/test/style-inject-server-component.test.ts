ec0c0a01d8f717461a2bba29320abd855b35101f
S.Watanabe <ocelot2828@gmail.com>
2025-03-30 11:52:58 +0900

    test: pass case is link transition

--- test-e2e/test/style-inject-server-component.test.ts
+++ test-e2e/test/style-inject-server-component.test.ts
@@ -3,8 +3,14 @@ import { test, expect } from '@playwright/test';
 test('Stylesheet is correctly inserted in the head at Server Component', async ({
   page,
 }) => {
-  await page.goto('http://localhost:4000/server');
+  // 1. initial page access
+  await page.goto('http://localhost:4000');
+
+  // 2. "Server Page" Link click and transition
+  await page.click('text=Server Page');
+
   await page.waitForSelector('[data-testid="e2e-test-p"]');
+
   const styleElements = await page.locator('head > style').count();
   expect(styleElements).toBeGreaterThan(0);
 
