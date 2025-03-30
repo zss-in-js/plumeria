e6fa96ef0928954424c76f478f3a087e041f4bb7
S.Watanabe <ocelot2828@gmail.com>
2025-03-30 11:17:18 +0900

    feat: import @plumeria/core/stylesheet

--- test-e2e/site/app/layout.tsx
+++ test-e2e/site/app/layout.tsx
@@ -1,6 +1,11 @@
 import { ServerCSS } from '@plumeria/next';
+import '@plumeria/core/stylesheet';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
