5e37279462ae5be575f93391ca55ba8839fc6681
S.Watanabe <ocelot2828@gmail.com>
2025-04-03 18:03:20 +0900

    feat: add stylesheet and vite-plugin

--- examples/with-vite-vue/vite.config.ts
+++ examples/with-vite-vue/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import plumeria from '@plumeria/vite';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), plumeria()],
 });
