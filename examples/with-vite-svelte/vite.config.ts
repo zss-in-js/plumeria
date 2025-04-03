fc165b1d0a7b667cbfead9bddd373078b5f7a1cb
S.Watanabe <ocelot2828@gmail.com>
2025-04-03 17:49:08 +0900

    feat(vite.config.ts): add plumeria-plugin

--- examples/with-vite-svelte/vite.config.ts
+++ examples/with-vite-svelte/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import plumeria from '@plumeria/vite';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), plumeria()],
 });
