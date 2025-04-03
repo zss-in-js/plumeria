1203d905611b9438e5b8b97c22e6546d601c09f6
S.Watanabe <ocelot2828@gmail.com>
2025-04-03 11:28:16 +0900

    feat: Replace @plumeria/collection with style-preset

--- packages/collection/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { colors } from './items/colors.js';
-export { container } from './items/container.js';
-export { media } from './items/media.js';
-export { pseudo } from './items/pseudo.js';
