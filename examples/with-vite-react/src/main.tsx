bfec0cbf359ac11f3fa0f4018b70c073e4e0b7d7
S.Watanabe <ocelot2828@gmail.com>
2025-04-03 17:46:22 +0900

    feat: add import @plumeria/core/stylesheet

--- examples/with-vite-react/src/main.tsx
+++ examples/with-vite-react/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@plumeria/core/stylesheet';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
