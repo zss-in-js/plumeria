bd9b6c3c066d76e7f7fc98b30ac9c22f48043104
S.Watanabe <ocelot2828@gmail.com>
2025-04-03 16:10:11 +0900

    feat: @plumeria vite-plugin

--- examples/with-vite-react/vite.config.ts
+++ examples/with-vite-react/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import plumeria from '@plumeria/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), plumeria()],
 });
