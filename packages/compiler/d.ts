2e58a2413e4541ff8fe65cc2005b1bdbc22af0a5
S.Watanabe <ocelot2828@gmail.com>
2025-04-06 20:43:23 +0900

    chore: removed import type

--- packages/compiler/d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@plumeria/core/dist/method/global-build-helper' {
-  export function buildGlobal(): Promise<void>;
-}
-
-declare module '@plumeria/core/dist/method/create-build-helper' {
-  export function buildCreate(): Promise<void>;
-}
