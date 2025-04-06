ee34bd1b56ea10312bf8a4e69dcca029863eccc0
S.Watanabe <ocelot2828@gmail.com>
2025-04-06 21:00:24 +0900

    feat: update compiler

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,25 +1,14 @@
-import { fileURLToPath } from 'url';
-import * as path from 'path';
-import * as fs from 'fs';
-import { execute } from 'rscute';
+import path from 'path';
+import fs from 'fs';
 import ts from 'typescript';
 import fg from 'fast-glob';
-import { buildCreate } from '@plumeria/core/dist/method/create-build-helper';
-import { buildGlobal } from '@plumeria/core/dist/method/global-build-helper';
-import postcss from 'postcss';
-import combineSelectors from 'postcss-combine-duplicated-selectors';
+import { execute } from 'rscute';
+import { createBuild } from 'zss-utils';
+import { globalBuild } from 'zss-utils';
 
-const cleanUp = async () => {
-  const projectRoot = process.cwd().split('node_modules')[0];
-  const directPath = path.join(projectRoot, 'node_modules/@plumeria/core');
-  const coreFilePath = path.join(directPath, 'stylesheet/core.css');
-
-  try {
-    fs.writeFileSync(coreFilePath, '', 'utf-8');
-  } catch (err) {
-    console.error('An error occurred:', err);
-  }
-};
+const projectRoot = process.cwd().split('node_modules')[0];
+const directPath = path.join(projectRoot, 'node_modules/@plumeria/core');
+const coreFilePath = path.join(directPath, 'stylesheet/core.css');
 
 function isCSS(filePath: string): boolean {
   const content = fs.readFileSync(filePath, 'utf8');
@@ -51,22 +40,12 @@ async function getAppRoot(): Promise<string> {
     : path.join(process.cwd(), '../../');
 }
 
-async function optimizeCSS() {
-  const corePackagePath = import.meta.resolve('@plumeria/core/package.json');
-  const corePath = path.dirname(fileURLToPath(new URL(corePackagePath)));
-  const cssPath = path.join(corePath, 'stylesheet/core.css');
-  const cssContent = fs.readFileSync(cssPath, 'utf8');
-  const result = postcss([
-    combineSelectors({ removeDuplicatedProperties: true }),
-  ]).process(cssContent, {
-    from: cssPath,
-    to: cssPath,
-  });
-  fs.writeFileSync(cssPath, result.css);
-}
-
 (async () => {
-  await cleanUp();
+  try {
+    fs.writeFileSync(coreFilePath, '', 'utf-8');
+  } catch (err) {
+    console.error('An error occurred:', err);
+  }
   const appRoot = await getAppRoot();
   const files = await fg([path.join(appRoot, '**/*.{js,jsx,ts,tsx}')], {
     ignore: [
@@ -81,10 +60,9 @@ async function optimizeCSS() {
     await execute(path.resolve(styleFiles[i]));
   }
   for (let i = 0; i < styleFiles.length; i++) {
-    await buildGlobal();
+    await globalBuild(coreFilePath);
   }
   for (let i = 0; i < styleFiles.length; i++) {
-    await buildCreate();
+    await createBuild(coreFilePath);
   }
-  await optimizeCSS();
 })();
