ba769eb157abc92554dd111332b750990cbd9606
S.Watanabe <ocelot2828@gmail.com>
2025-04-14 17:31:59 +0900

    feat(with-vite-react): update compiler it's faster

--- examples/with-vite-react/src/style.tsx
+++ examples/with-vite-react/src/style.tsx
@@ -4,6 +4,6 @@ export const styles = css.create({
   color: {
     WebkitTextFillColor: 'transparent',
     background: 'linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)',
-    WebkitBackgroundClip: 'text',
+    backgroundClip: 'text',
   },
 });
