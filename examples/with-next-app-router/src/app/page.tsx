af967a201e2e2086fc5ded24b76d6a6e326444f8
S.Watanabe <ocelot2828@gmail.com>
2025-04-14 21:57:40 +0900

    feat(with-vite-react): update package

--- examples/with-next-app-router/src/app/page.tsx
+++ examples/with-next-app-router/src/app/page.tsx
@@ -4,7 +4,6 @@ import { css } from '@plumeria/core';
 
 const style = css.create({
   color: {
-    fontSize: 40,
     WebkitTextFillColor: 'transparent',
     background: 'linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)',
     backgroundClip: 'text',
