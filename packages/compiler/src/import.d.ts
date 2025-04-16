be21a50017989d81cd7442ad978ac7d0674058ef
S.Watanabe <ocelot2828@gmail.com>
2025-04-16 23:12:24 +0900

    fix(ci): build-helper Import directly via core

---
+++ packages/compiler/src/import.d.ts
@@ -0,0 +1,4 @@
+declare module '@plumeria/core/build-helper' {
+  export declare function buildCreate(filePath: string): Promise<void>;
+  export declare function buildGlobal(filePath: string): Promise<void>;
+}
