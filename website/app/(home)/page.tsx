2edf32d28adb3c681771ddfbc40e095a5b4a1ff6
S.Watanabe <ocelot2828@gmail.com>
2025-04-17 11:22:15 +0900

    feat: top z-index

--- website/app/(home)/page.tsx
+++ website/app/(home)/page.tsx
@@ -1,14 +1,21 @@
 import { HomeComponent } from '@/component/HomeComponent';
 import { Metadata } from 'next';
 import { CodeBlock } from '@/component/CodeBlock';
+import { css } from '@plumeria/core';
 
 export const metadata: Metadata = {
   title: 'Plumeria - Zero-runtime CSS in JS library',
 };
 
+const styles = css.create({
+  container: {
+    position: 'relative',
+    zIndex: 0,
+  },
+});
 export default function Page() {
   return (
-    <div>
+    <div className={styles.container}>
       <HomeComponent />
       <CodeBlock />
     </div>
