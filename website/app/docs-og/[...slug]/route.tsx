d361a9cc3eb503ed432327aab762d7eb13787e36
S.Watanabe <ocelot2828@gmail.com>
2025-04-17 16:32:14 +0900

    fix: import path

--- website/app/docs-og/[...slug]/route.tsx
+++ website/app/docs-og/[...slug]/route.tsx
@@ -1,11 +1,11 @@
 import { generateOGImage } from 'fumadocs-ui/og';
-import { metadataImage } from '../../../lib/metadata';
+import { metadataImage } from 'lib/metadata';
 
 export const GET = metadataImage.createAPI((page) => {
   return generateOGImage({
     title: page.data.title,
     description: page.data.description,
-    site: '@plumeria',
+    site: 'Plumeria',
   });
 });
 
