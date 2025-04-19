768ebb01d413419a2d00e2d7fd4b40b47fcef139
S.Watanabe <ocelot2828@gmail.com>
2025-04-19 10:23:28 +0900

    feat(vite-plugin): package name update

--- examples/with-vite-svelte/vite.config.ts
+++ examples/with-vite-svelte/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import plumeria from '@plumeria/vite';
+import plumeria from '@plumeria/vite-plugin';
 
 export default defineConfig({
   plugins: [svelte(), plumeria()],
--- examples/with-vite-vue/vite.config.ts
+++ examples/with-vite-vue/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import plumeria from '@plumeria/vite';
+import plumeria from '@plumeria/vite-plugin';
 
 export default defineConfig({
   plugins: [vue(), plumeria()],
