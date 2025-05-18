cbea17e888182dc1deefbde69dd8efaabff99137
refirst11 <ocelot2828@gmail.com>
2025-05-18 13:21:14 +0900

    feat(vars.ts): Synchronize with documents

--- test-e2e/site/component/vars.ts
+++ test-e2e/site/component/vars.ts
@@ -2,4 +2,30 @@ import { css } from '@plumeria/core';
 
 export const utils = css.defineVars({
   mini: '12px',
+  medium: '16px',
+  large: '24px',
+});
+
+// Synchronize with documents
+export const tokens = css.defineVars({
+  white: 'white',
+  black: 'black',
+  textPrimary: '#eaeaea',
+  textSecondary: '#333',
+  link: 'lightblue',
+  accent: 'purple',
+});
+
+tokens.textPrimary;
+
+export const theme = css.defineTheme({
+  textPrimary: {
+    default: 'rgb(60,60,60)',
+    light: 'black',
+    dark: 'white',
+  },
+  bgPrimary: {
+    light: 'white',
+    dark: 'black',
+  },
 });
