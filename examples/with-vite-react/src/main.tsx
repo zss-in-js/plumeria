9aae39c1e6c71bbb855735f94c79a4900c66ea9b
refirst11 <ocelot2828@gmail.com>
2025-05-25 11:43:18 +0900

    feat(examples): plumeria core and compiler update

--- examples/with-vite-react/src/main.tsx
+++ examples/with-vite-react/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import '@plumeria/core/stylesheet';
+import '@plumeria/core/stylesheet.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
