b5de05abebb0f68519bed83271e0380b48bc3634
refirst11 <ocelot2828@gmail.com>
2025-06-06 21:00:33 +0900

    feat: export add px and ps and color

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,6 @@
 export { default as css } from './css';
 export { default as cx } from './cx';
+export { default as px } from './px';
 export { default as rx } from './rx';
 export type { CSSProperties, CSSHTML, CreateStyle } from './css';
+export { ps, color } from 'zss-utils';
