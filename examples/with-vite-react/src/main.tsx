baff0ecc4450394a1e9ebe9aaceaa692b2e23f44
refirst11 <ocelot2828@gmail.com>
2025-06-10 11:10:24 +0900

    chore(with-vite-react): update npm create vite --template react-ts

--- examples/with-vite-react/src/main.tsx
+++ examples/with-vite-react/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App.tsx';
 import '@plumeria/core/stylesheet.css';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
