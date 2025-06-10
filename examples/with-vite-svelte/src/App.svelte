03fe8401f5dc047c6f18ed5e5f0441bc359d7ed7
refirst11 <ocelot2828@gmail.com>
2025-06-10 11:03:36 +0900

    chore(with-vite-svelte): update npm create vite --template svelte-ts

--- examples/with-vite-svelte/src/App.svelte
+++ examples/with-vite-svelte/src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
@@ -7,7 +7,7 @@
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+    <a href="https://vite.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
@@ -29,3 +29,20 @@
   </p>
 </main>
 
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
--- examples/with-vite-svelte/vite.config.ts
+++ examples/with-vite-svelte/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import plumeria from '@plumeria/vite';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), plumeria()],
 });
