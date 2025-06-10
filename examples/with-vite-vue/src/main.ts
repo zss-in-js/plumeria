72fed6b3d9351d4b076092b618055f9ed6066bf1
refirst11 <ocelot2828@gmail.com>
2025-06-10 10:50:44 +0900

    chore(with-vite-vue): update npm create vite --template vue-ts

--- examples/with-vite-vue/src/main.ts
+++ examples/with-vite-vue/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import './style.css';
 import '@plumeria/core/stylesheet.css';
+import App from './App.vue';
 
 createApp(App).mount('#app');
--- examples/with-vite-vue/vite.config.ts
+++ examples/with-vite-vue/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import plumeria from '@plumeria/vite';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), plumeria()],
 });
