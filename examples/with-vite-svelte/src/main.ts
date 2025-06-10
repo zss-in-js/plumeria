51db2398c7d8312530cb1dab50fc68b2858316cc
refirst11 <ocelot2828@gmail.com>
2025-06-10 11:03:36 +0900

    chore(with-vite-svelte): update npm create vite --template svelte-ts

--- examples/with-vite-svelte/src/main.ts
+++ examples/with-vite-svelte/src/main.ts
@@ -1,8 +1,9 @@
+import { mount } from 'svelte';
 import './app.css';
 import App from './App.svelte';
 import '@plumeria/core/stylesheet.css';
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app') as HTMLElement,
 });
 
