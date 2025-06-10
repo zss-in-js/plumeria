b6595792633c2c95a5cf9a163110bb41f4c384af
refirst11 <ocelot2828@gmail.com>
2025-06-10 20:24:05 +0900

    chore(with-react-reouter-v7):  fix eslint

--- examples/with-react-router-v7/app/routes/home.tsx
+++ examples/with-react-router-v7/app/routes/home.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-empty-pattern */
+/* eslint-disable @plumeria/validate-values */
 import type { Route } from './+types/home';
 import { Welcome } from '../welcome/welcome';
 
-// eslint-disable-next-line no-empty-pattern
 export function meta({}: Route.MetaArgs) {
   return [
     { title: 'New React Router App' },
