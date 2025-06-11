c40d33c7336150c96737772773ab283665d31788
refirst11 <ocelot2828@gmail.com>
2025-06-11 16:49:51 +0900

    feat(source.config.ts): docs.schema add .extend date

--- website/source.config.ts
+++ website/source.config.ts
@@ -1,4 +1,5 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig, frontmatterSchema } from 'fumadocs-mdx/config';
+import { z } from 'zod';
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
@@ -6,6 +7,13 @@ export const { docs, meta } = defineDocs({
 
 export const { docs: blogDocs, meta: blogMeta } = defineDocs({
   dir: 'content/blog',
+  docs: {
+    schema: frontmatterSchema
+      .extend({
+        date: z.string(),
+      })
+      .strict(),
+  },
 });
 
 export default defineConfig({
