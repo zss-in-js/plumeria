6890dfe7e8ebdd6eeccad4a6e0f7299d7a4d6c42
refirst11 <ocelot2828@gmail.com>
2025-06-15 13:24:35 +0900

    reafactor(core): remove files cx ps px rx

--- packages/core/src/cx.ts
+++
@@ -1,4 +0,0 @@
-const cx = (...classes: Array<string | null | undefined | false>): string =>
-  [...new Set(classes.filter(Boolean))].join(' ');
-
-export default cx;
--- packages/core/src/ps.ts
+++
@@ -1,3 +0,0 @@
-import { ps } from 'zss-utils';
-
-export default ps;
