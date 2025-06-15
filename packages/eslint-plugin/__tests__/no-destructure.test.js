fea161634fe77f75dc5bbd4acbe58367a3c569a8
refirst11 <ocelot2828@gmail.com>
2025-06-15 12:23:08 +0900

    test(no-destructure.test.js): add

---
+++ packages/eslint-plugin/__tests__/no-destructure.test.js
@@ -0,0 +1,65 @@
+const { RuleTester } = require('eslint');
+const rule = require('../lib/rules/no-destructure');
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-destructure', rule, {
+  valid: [
+    {
+      code: 'const { keyframes } = css',
+      code: 'const { defineConsts } = css',
+      code: 'const { defineVars } = css',
+      code: 'const { defineTheme } = css',
+      code: 'const { rx } = css',
+      code: 'const { px } = css',
+      code: 'const { media } = css',
+      code: 'const { container } = css',
+      code: 'const { color } = css;',
+      settings: {
+        ecmaVersion: 2021,
+      },
+    },
+  ],
+  invalid: [
+    {
+      code: 'const { create } = css;',
+      errors: [
+        {
+          message:
+            'Do not destructure "create" from "css". Use dot notation instead.',
+        },
+      ],
+      settings: {
+        ecmaVersion: 2021,
+      },
+    },
+    {
+      code: 'const { global } = css;',
+      errors: [
+        {
+          message:
+            'Do not destructure "global" from "css". Use dot notation instead.',
+        },
+      ],
+      settings: {
+        ecmaVersion: 2021,
+      },
+    },
+    {
+      code: 'const { create, global } = css;',
+      errors: [
+        {
+          message:
+            'Do not destructure "create" from "css". Use dot notation instead.',
+        },
+        {
+          message:
+            'Do not destructure "global" from "css". Use dot notation instead.',
+        },
+      ],
+      settings: {
+        ecmaVersion: 2021,
+      },
+    },
+  ],
+});
