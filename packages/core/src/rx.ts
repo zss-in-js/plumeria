6d0a300cd68ed562c651978660ba4ee91c4345a9
refirst11 <ocelot2828@gmail.com>
2025-06-15 13:24:35 +0900

    reafactor(core): remove files cx ps px rx

--- packages/core/src/rx.ts
+++
@@ -1,8 +0,0 @@
-const rx = (className: string, varSet: { [key: `--${string}`]: string }) => ({
-  className,
-  style: Object.fromEntries(
-    Object.entries(varSet).map(([key, value]) => [key, value]),
-  ),
-});
-
-export default rx;
