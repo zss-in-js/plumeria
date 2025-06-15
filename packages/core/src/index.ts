f823d73603cdecd5b9caed09e5bab34b4df217e1
refirst11 <ocelot2828@gmail.com>
2025-06-15 13:27:39 +0900

    feat(core/index.ts): export update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,2 @@
-export { default as css } from './css';
-export { default as cx } from './cx';
-export { default as ps } from './ps';
-export { default as px } from './px';
-export { default as rx } from './rx';
-export type { CSSProperties, CSSHTML, CreateStyle } from './css';
+export { css, ps, px, rx } from './css';
+export type { CreateStyle, CSSHTML, CSSProperties } from './css';
