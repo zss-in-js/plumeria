27654c9cde79032420a431bbdb69021284a0851c
refirst11 <ocelot2828@gmail.com>
2025-06-29 21:26:34 +0900

    feat(core): update: Returns the className directly with the dollar sign

--- packages/core/src/main/create.ts
+++ packages/core/src/main/create.ts
@@ -21,22 +21,35 @@ function create<const T extends Record<string, CSSProperties>>(
   const base36Hash = genBase36Hash(object, 6);
   const { styleSheet } = transpiler(object, base36Hash);
   const injectCSS = isServer ? injectServerCSS : injectClientCSS;
+  const injectIfNeeded = () => {
+    if (isTestingDevelopment) injectCSS(base36Hash, styleSheet);
+  };
   if (typeof globalPromise_1 === 'undefined') initPromise_1();
   resolvePromise_1(styleSheet);
+
+  const result = {};
 
   Object.keys(object).forEach((key) => {
     const cssProperties = object[key];
     const hashedClassName = key + '_' + base36Hash;
     objectToKeyHashMap.set(cssProperties, hashedClassName);
-    Object.defineProperty(object, key, {
+
+    Object.defineProperty(result, key, {
       get: () => {
-        if (isTestingDevelopment) injectCSS(base36Hash, styleSheet);
+        injectIfNeeded();
         return Object.freeze(cssProperties);
+      },
+    });
+
+    Object.defineProperty(result, '$' + key, {
+      get: () => {
+        injectIfNeeded();
+        return hashedClassName;
       },
     });
   });
 
-  return Object.freeze(object as ReturnType<T>);
+  return Object.freeze(result) as ReturnType<T>;
 }
 
 export { create, objectToKeyHashMap };
