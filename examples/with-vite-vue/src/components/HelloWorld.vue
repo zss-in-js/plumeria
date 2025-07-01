c7e6cd0d63db962ae9d27318deb92a8d7992c871
refirst11 <ocelot2828@gmail.com>
2025-07-01 10:46:48 +0900

    feat(with-vite-vue/src/components): $ accessor

--- examples/with-vite-vue/src/components/HelloWorld.vue
+++ examples/with-vite-vue/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { styles } from '../style';
-import { css } from '@plumeria/core';
 
 defineProps<{ msg: string }>();
 
@@ -9,7 +8,7 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1 :class="css.props(styles.color)">{{ msg }}</h1>
+  <h1 :class="styles.$color">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
