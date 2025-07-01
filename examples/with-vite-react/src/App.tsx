d20a927a0fd78390fa501a712e080681da9dc31c
refirst11 <ocelot2828@gmail.com>
2025-07-01 10:43:04 +0900

    feat(with-vite-react/src): $ accessor

--- examples/with-vite-react/src/App.tsx
+++ examples/with-vite-react/src/App.tsx
@@ -25,9 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <div className={css.props(styles.color)}>
-        Vite + React + Plumeria (SWC)
-      </div>
+      <div className={styles.$color}>Vite + React + Plumeria (SWC)</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
