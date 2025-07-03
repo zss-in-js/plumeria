59dec7f4392f2513b6dd5139b6192935868f234a
refirst11 <ocelot2828@gmail.com>
2025-07-03 15:47:35 +0900

    feat(core/keyframes.ts): hash func update

--- packages/core/src/define/keyframes.ts
+++ packages/core/src/define/keyframes.ts
@@ -3,7 +3,7 @@ import { genBase36Hash } from 'zss-engine';
 import { global } from '../main/global';
 
 const keyframes = (object: CreateKeyframes): string => {
-  const prefix = genBase36Hash(object, 8);
+  const prefix = genBase36Hash(object, 1, 8);
   global({ [`@keyframes ${prefix}`]: object });
   return prefix;
 };
