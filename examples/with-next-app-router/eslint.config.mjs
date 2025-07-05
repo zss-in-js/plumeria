5e17ceb3c9c55da3f507e84dd52136ca475f6208
refirst11 <ocelot2828@gmail.com>
2025-07-05 15:04:28 +0900

    feat(examples/with-next-app-router/eslint.config.mjs): add @plumeria/eslint-plugin

--- examples/with-next-app-router/eslint.config.mjs
+++ examples/with-next-app-router/eslint.config.mjs
@@ -1,5 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import zsslint from 'eslint-plugin-zss-lint';
+import plumeria from '@plumeria/eslint-plugin';
 
 const __dirname = import.meta.dirname;
 
@@ -12,10 +12,10 @@ const eslintConfig = [
   {
     files: ['**/*.{ts,js,jsx,tsx}'],
     plugins: {
-      'zss-lint': zsslint,
+      '@plumeria': plumeria,
     },
   },
-  ...compat.extends('plugin:zss-lint/recommended'),
+  ...compat.extends('plugin:@plumeria/recommended'),
 ];
 
 export default eslintConfig;
