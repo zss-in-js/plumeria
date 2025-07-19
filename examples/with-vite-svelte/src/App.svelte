b99b6ff41954a6e25bd966364b7747283f3a9f46
refirst11 <ocelot2828@gmail.com>
2025-07-19 18:13:33 +0900

    feat(examples/svelte): update

--- examples/with-vite-svelte/src/App.svelte
+++ examples/with-vite-svelte/src/App.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import { styles } from './style'
+  import svelteLogo from './assets/svelte.svg';
+  import viteLogo from '/vite.svg';
+  import Counter from './lib/Counter.svelte';
+  import { css } from '@plumeria/core';
+
+  const styles = css.create({
+    color: {
+      WebkitTextFillColor: 'transparent',
+      background:
+        'linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)',
+      backgroundClip: 'text',
+    },
+  });
 </script>
 
 <main>
@@ -14,19 +23,21 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1 class={styles.$color} >Vite + Svelte + Plumeria</h1>
+  <h1 class={css.props(styles.color)}>Vite + Svelte + Plumeria</h1>
 
   <div class="card">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
