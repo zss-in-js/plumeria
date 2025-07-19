58d5b7977d643c54844e735a139bee07ed3e1025
refirst11 <ocelot2828@gmail.com>
2025-07-19 18:22:05 +0900

    feat(core/index.ts): static assets require import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,5 @@
-export { css, ps, px, rx } from './css';
+export { rx } from './api/utils/rx';
+export { px } from './api/utils/px';
+export { media, container, color, ps } from 'zss-utils';
+export { css } from './css';
 export type { CreateStyle, CSSHTML, CSSProperties } from './css';
