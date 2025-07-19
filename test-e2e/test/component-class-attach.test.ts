9d03de44f6f844940afa94f057ee176bf11b7cb1
refirst11 <ocelot2828@gmail.com>
2025-07-19 18:55:44 +0900

    test(component-class-attach.test.ts): fix hashes

--- test-e2e/test/component-class-attach.test.ts
+++ test-e2e/test/component-class-attach.test.ts
@@ -4,5 +4,5 @@ test('Components have correct classes applied', async ({ page }) => {
   await page.goto('http://localhost:4000');
   await page.waitForSelector('[data-testid="e2e-test-div"]');
   const div = page.locator('[data-testid="e2e-test-div"]');
-  await expect(div).toHaveClass('s13vu618 c15qdpgc');
+  await expect(div).toHaveClass('z13vu618 z15qdpgc');
 });
