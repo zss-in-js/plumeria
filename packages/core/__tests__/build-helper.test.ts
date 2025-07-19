f3f441ed24f4eb4fb0e9b2f81dc64ea7cca937ea
refirst11 <ocelot2828@gmail.com>
2025-07-19 18:47:23 +0900

    fix(core/__test__/build-helper.test.ts): update

--- packages/core/__tests__/build-helper.test.ts
+++ packages/core/__tests__/build-helper.test.ts
@@ -4,7 +4,8 @@ import { css } from '../src/css';
 
 test('set function should create globalPromise and add styles to it', () => {
   expect(globalPromise_1).toBeUndefined();
-  css.create({ test: { color: 'red' } });
+  const styles = css.create({ test: { color: 'red' } });
+  css.props(styles.test);
   expect(globalPromise_1).toBeDefined();
   expect(globalPromise_1).resolves.toContain('color: red');
 });
