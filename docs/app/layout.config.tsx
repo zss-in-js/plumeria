3ddcd42dda72f003afc41dc8354f70e551a0bca4
refirst11 <ocelot2828@gmail.com>
2025-07-20 20:16:42 +0900

    feat(layout.config.tsx): remove slash

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -7,27 +7,14 @@ const styles = css.create({
   logo: {
     display: 'flex',
     flexDirection: 'row',
-    gap: 14,
+    gap: 12,
     fontWeight: 700,
   },
   zss: {
     borderRadius: '8px',
-  },
-  slash: {
-    fontSize: 16,
-    fontWeight: '400',
-    color: 'rgb(170 170 170 / 0.3)',
-    rotate: '0deg',
+    scale: 0.88,
   },
 });
-
-/**
- * Shared layout configurations
- *
- * you can configure layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
- */
 
 export const baseOptions: BaseLayoutProps = {
   githubUrl: 'https://github.com/zss-in-js/plumeria',
@@ -35,7 +22,7 @@ export const baseOptions: BaseLayoutProps = {
     title: (
       <span className={css.props(styles.logo)}>
         <Image src="/zss.png" alt="Plumeria logo" height={24} width={24} className={css.props(styles.zss)} />
-        <span className={css.props(styles.slash)}>/</span> ✾ Plumeria
+        Plumeria
       </span>
     ),
     transparentMode: 'top',
