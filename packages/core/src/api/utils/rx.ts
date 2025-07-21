5ace4b6f04eb492ab35b27f0d18ec6b8973f3cf2
refirst11 <ocelot2828@gmail.com>
2025-07-21 13:08:41 +0900

    fix(core/rx.ts): The argument takes a string directly

--- packages/core/src/api/utils/rx.ts
+++ packages/core/src/api/utils/rx.ts
@@ -1,19 +1,10 @@
-import type { CSSProperties, RxVariableSet } from 'zss-engine';
-import { props } from '../../api/props';
+import type { RxVariableSet } from 'zss-engine';
 
-const rx = (
-  styleProps: string | Readonly<CSSProperties>,
-  varSet: RxVariableSet,
-) => {
-  const className =
-    typeof styleProps === 'string' ? styleProps : props(styleProps);
-
-  return {
-    className,
-    style: Object.fromEntries(
-      Object.entries(varSet).map(([key, value]) => [key, value]),
-    ),
-  };
-};
+const rx = (className: string, varSet: RxVariableSet) => ({
+  className,
+  style: Object.fromEntries(
+    Object.entries(varSet).map(([key, value]) => [key, value]),
+  ),
+});
 
 export { rx };
