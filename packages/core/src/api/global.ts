27c3909704ac16c27f88adca4dbba1b3c8b17882
refirst11 <ocelot2828@gmail.com>
2025-08-10 04:31:53 +0900

    fix(global.ts): injectServerCSS is has been removed

--- packages/core/src/api/global.ts
+++ packages/core/src/api/global.ts
@@ -3,9 +3,7 @@ import {
   transpile,
   isServer,
   isTestingDevelopment,
-  injectServerCSS,
   injectClientGlobalCSS,
-  genBase36Hash,
 } from 'zss-engine';
 import {
   initPromise_2,
@@ -14,15 +12,12 @@ import {
 } from '../processors/css';
 
 function global(object: CSSHTML): void {
-  const base36Hash = genBase36Hash(object, 1, 8);
   const { styleSheet } = transpile(object, undefined, '--global');
+
   if (typeof globalPromise_2 === 'undefined') initPromise_2();
   resolvePromise_2(styleSheet);
 
-  if (isTestingDevelopment)
-    isServer
-      ? injectServerCSS(base36Hash, styleSheet)
-      : injectClientGlobalCSS(styleSheet);
+  if (isTestingDevelopment && !isServer) injectClientGlobalCSS(styleSheet);
 }
 
 export { global };
