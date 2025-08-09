de47b9fcba08ae1f3bbfbca3d10d65cc7c976730
refirst11 <ocelot2828@gmail.com>
2025-08-10 00:18:40 +0900

    fix(next): has been removed

--- packages/next/src/component/isDevtools.ts
+++
@@ -1,9 +0,0 @@
-export function isDevTools(): boolean {
-  const devtools = /./;
-  (devtools as any).toString = function () {
-    this.opened = true;
-  };
-  String(devtools);
-
-  return Boolean((devtools as any).opened);
-}
