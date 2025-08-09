c8352c54d124b779d60097c06d62d9f2d4a934ba
refirst11 <ocelot2828@gmail.com>
2025-08-10 04:32:25 +0900

    test(core/__tests__/all): updated

--- packages/core/__tests__/global.test.ts
+++ packages/core/__tests__/global.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @plumeria/no-inner-call */
-import { css } from '../src/css';
+import { css } from '../dist/index.js';
 
 test('global return value is undefined"', () => {
   const result = css.global({ h1: { fontSize: 24 } });
