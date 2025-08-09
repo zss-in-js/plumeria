f835c1bedc66ea4ef2ea9e379128e354ef682791
refirst11 <ocelot2828@gmail.com>
2025-08-10 00:21:29 +0900

    refactor(core/keyframes.ts): const variable prefix to  hash

--- packages/core/src/api/keyframes.ts
+++ packages/core/src/api/keyframes.ts
@@ -3,9 +3,9 @@ import { genBase36Hash } from 'zss-engine';
 import { global } from './global';
 
 const keyframes = (object: CreateKeyframes): string => {
-  const prefix = genBase36Hash(object, 1, 8);
-  global({ [`@keyframes ${prefix}`]: object });
-  return prefix;
+  const hash = genBase36Hash(object, 1, 8);
+  global({ [`@keyframes ${hash}`]: object });
+  return hash;
 };
 
 export { keyframes };
