c9a3ed32390522465257024d8ee7c9811f078f41
refirst11 <ocelot2828@gmail.com>
2025-08-13 17:06:30 +0900

    feat(webpack-plugin/index.ts): removed a HMR bug in the empty string guard

--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,64 +1,80 @@
 import type { Compiler } from 'webpack';
-import { Compilation, sources } from 'webpack';
-import VirtualModulesPlugin from 'webpack-virtual-modules';
+import path from 'path';
+import fs from 'fs';
 
 const PLUGIN_NAME = 'PlumeriaPlugin';
-
 export class PlumeriaPlugin {
   private stylesByFile = new Map<string, any>();
-  private virtualModules?: VirtualModulesPlugin;
+  private outFile!: string;
+
+  constructor(private outputFileName = 'zero-virtual.css') {}
 
   apply(compiler: Compiler) {
-    this.virtualModules = new VirtualModulesPlugin();
-    this.virtualModules.apply(compiler);
+    this.outFile = path.resolve(__dirname, '..', this.outputFileName);
 
     compiler.hooks.compile.tap(PLUGIN_NAME, () => {
       this.stylesByFile.clear();
     });
 
-    compiler.hooks.thisCompilation.tap(PLUGIN_NAME, (compilation) => {
-      compilation.hooks.processAssets.tapPromise(
-        {
-          name: PLUGIN_NAME,
-          stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
-        },
-        async () => {
-          const css = this.generateOrderedCSS(this.stylesByFile);
-          compilation.emitAsset('plumeria.css', new sources.RawSource(css));
-        },
-      );
+    compiler.hooks.normalModuleFactory.tap(PLUGIN_NAME, (nmf) => {
+      nmf.hooks.createModule.tap(PLUGIN_NAME, (createData) => {
+        const modPath =
+          createData.matchResource ?? createData.resourceResolveData?.path;
+
+        if (modPath && modPath.includes('zero-virtual.css')) {
+          createData.settings ??= {};
+          createData.settings.sideEffects = true; // Now webpack won't tree-shake
+        }
+      });
     });
   }
 
-  registerStyle(virtualFileName: string, styles: any) {
-    this.stylesByFile.set(virtualFileName, styles);
-    const css = this.generateOrderedCSS(this.stylesByFile);
-    this.virtualModules?.writeModule(virtualFileName, css);
+  registerFileStyles(filePath: string, styles: Partial<any>) {
+    const prev = this.stylesByFile.get(filePath) || {
+      filePath,
+      globalStyles: '',
+      keyframeStyles: '',
+      varStyles: '',
+      themeStyles: '',
+      baseStyles: '',
+    };
+    this.stylesByFile.set(filePath, { ...prev, ...styles });
+    this.writeCSS();
   }
 
-  private generateOrderedCSS(styles: any): string {
-    const allStyles = Array.from(styles.values()) as any;
-    const globalStyles: string[] = [];
+  private generateOrderedCSS(): string {
+    const allStyles = Array.from(this.stylesByFile.values());
+
+    const globalStylesSet = new Set<string>();
     const keyframeStylesSet = new Set<string>();
     const varStylesSet = new Set<string>();
     const themeStylesSet = new Set<string>();
-    const baseStyles: string[] = [];
+    const baseStylesSet = new Set<string>();
 
     for (const s of allStyles) {
-      if (s.globalStyles) globalStyles.push(s.globalStyles);
+      if (s.globalStyles) globalStylesSet.add(s.globalStyles);
       if (s.keyframeStyles) keyframeStylesSet.add(s.keyframeStyles);
       if (s.varStyles) varStylesSet.add(s.varStyles);
       if (s.themeStyles) themeStylesSet.add(s.themeStyles);
-      if (s.baseStyles) baseStyles.push(s.baseStyles);
+      if (s.baseStyles) baseStylesSet.add(s.baseStyles);
     }
+
     return [
-      ...globalStyles,
+      ...Array.from(globalStylesSet),
       ...Array.from(keyframeStylesSet),
       ...Array.from(varStylesSet),
       ...Array.from(themeStylesSet),
-      ...baseStyles,
+      ...Array.from(baseStylesSet),
     ]
       .filter(Boolean)
       .join('\n');
+  }
+
+  private writeCSS() {
+    const css = this.generateOrderedCSS();
+    if (css.trim().length > 0) {
+      fs.mkdirSync(path.dirname(this.outFile), { recursive: true });
+      fs.writeFileSync(this.outFile, css, 'utf-8');
+    }
   }
 }
