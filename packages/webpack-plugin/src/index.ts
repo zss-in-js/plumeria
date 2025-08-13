e6496387e1bdbd7a20a75d7d04defa86f2e42c4c
refirst11 <ocelot2828@gmail.com>
2025-08-13 15:44:52 +0900

    feat(webpack-plugin): update use virtual-css-modules and rimraf for a clean setup

--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,63 +1,43 @@
-import { Compilation } from 'webpack';
 import type { Compiler } from 'webpack';
-import path from 'path';
-import fs from 'fs';
+import { Compilation, sources } from 'webpack';
+import VirtualModulesPlugin from 'webpack-virtual-modules';
 
 const PLUGIN_NAME = 'PlumeriaPlugin';
+
 export class PlumeriaPlugin {
   private stylesByFile = new Map<string, any>();
-  private outFile!: string;
-
-  constructor(private outputFileName = 'zero-virtual.css') {}
+  private virtualModules?: VirtualModulesPlugin;
 
   apply(compiler: Compiler) {
-    this.outFile = path.resolve(__dirname, '..', this.outputFileName);
+    this.virtualModules = new VirtualModulesPlugin();
+    this.virtualModules.apply(compiler);
 
     compiler.hooks.compile.tap(PLUGIN_NAME, () => {
       this.stylesByFile.clear();
     });
 
-    compiler.hooks.normalModuleFactory.tap(PLUGIN_NAME, (nmf) => {
-      nmf.hooks.createModule.tap(PLUGIN_NAME, (createData) => {
-        const modPath =
-          createData.matchResource ?? createData.resourceResolveData?.path;
-
-        if (modPath && modPath.includes('zero-virtual.css')) {
-          createData.settings ??= {};
-          createData.settings.sideEffects = true;
-        }
-      });
-    });
-
     compiler.hooks.thisCompilation.tap(PLUGIN_NAME, (compilation) => {
-      compilation.hooks.processAssets.tap(
+      compilation.hooks.processAssets.tapPromise(
         {
           name: PLUGIN_NAME,
           stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
         },
-        () => {
-          this.writeCSS();
+        async () => {
+          const css = this.generateOrderedCSS(this.stylesByFile);
+          compilation.emitAsset('plumeria.css', new sources.RawSource(css));
         },
       );
     });
   }
 
-  registerFileStyles(filePath: string, styles: Partial<any>) {
-    const prev = this.stylesByFile.get(filePath) || {
-      filePath,
-      globalStyles: '',
-      keyframeStyles: '',
-      varStyles: '',
-      themeStyles: '',
-      baseStyles: '',
-    };
-    this.stylesByFile.set(filePath, { ...prev, ...styles });
-    this.writeCSS();
+  registerStyle(virtualFileName: string, styles: any) {
+    this.stylesByFile.set(virtualFileName, styles);
+    const css = this.generateOrderedCSS(this.stylesByFile);
+    this.virtualModules?.writeModule(virtualFileName, css);
   }
 
-  private generateOrderedCSS(): string {
-    const allStyles = Array.from(this.stylesByFile.values());
-
+  private generateOrderedCSS(styles: any): string {
+    const allStyles = Array.from(styles.values()) as any;
     const globalStyles: string[] = [];
     const keyframeStylesSet = new Set<string>();
     const varStylesSet = new Set<string>();
@@ -71,7 +51,6 @@ export class PlumeriaPlugin {
       if (s.themeStyles) themeStylesSet.add(s.themeStyles);
       if (s.baseStyles) baseStyles.push(s.baseStyles);
     }
-
     return [
       ...globalStyles,
       ...Array.from(keyframeStylesSet),
@@ -81,12 +60,5 @@ export class PlumeriaPlugin {
     ]
       .filter(Boolean)
       .join('\n');
-  }
-
-  private writeCSS() {
-    const css = this.generateOrderedCSS();
-
-    fs.mkdirSync(path.dirname(this.outFile), { recursive: true });
-    fs.writeFileSync(this.outFile, css, 'utf-8');
   }
 }
