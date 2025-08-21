b9a0dd0443ee8f1f448ac3998ae0b0d452cd7b36
refirst11 <ocelot2828@gmail.com>
2025-08-21 14:39:59 +0900

    examples: @plumeria@0.15.7 update

--- examples/with-react-router-v7/vite.config.ts
+++ examples/with-react-router-v7/vite.config.ts
@@ -2,7 +2,7 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import plumeria from '@plumeria/vite';
+import plumeria from '@plumeria/vite-plugin';
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), plumeria()],
