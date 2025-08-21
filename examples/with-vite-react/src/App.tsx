8f032e28c3fce8402a85e25edc1d67b6806aa6d8
refirst11 <ocelot2828@gmail.com>
2025-08-21 14:39:59 +0900

    examples: @plumeria@0.15.7 update

--- examples/with-vite-react/src/App.tsx
+++ examples/with-vite-react/src/App.tsx
@@ -7,7 +7,7 @@ import { css } from '@plumeria/core';
 const styles = css.create({
   color: {
     WebkitTextFillColor: 'transparent',
-    background: 'linear-gradient(90deg, #58c6ff 0%, #076ad9 50%, #ff3bef 100%)',
+    background: 'linear-gradient(90deg, #58c6ff 0%, #3e5875 50%, #ff3bef 100%)',
     backgroundClip: 'text',
   },
 });
