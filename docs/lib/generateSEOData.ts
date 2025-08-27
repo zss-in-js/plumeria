1f93effc653361b6a65a05f56742b7134069364a
refirst11 <ocelot2828@gmail.com>
2025-08-27 11:48:39 +0900

    feat(docs): ogp update

---
+++ docs/lib/generateSEOData.ts
@@ -0,0 +1,39 @@
+type SeoData = {
+  title: string;
+  subtitle?: string;
+  date?: string;
+};
+
+const generateSEOData = ({ title, subtitle, date }: SeoData) => {
+  return {
+    title: title,
+    description: subtitle,
+    metadataBase: new URL(process.env.PROD_URL || 'https://.io'),
+    alternates: {
+      canonical: '/',
+    },
+    openGraph: {
+      title: title,
+      description: subtitle,
+      url: process.env.PROD_URL,
+      type: 'website',
+      images: {
+        width: 1200,
+        height: 600,
+        url: `${process.env.PROD_URL}/api/ogp?title=${title}&date=${date}`,
+      },
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: title,
+      description: subtitle,
+      images: {
+        width: 1200,
+        height: 600,
+        url: `${process.env.PROD_URL}/api/ogp?title=${title}&date=${date}`,
+      },
+    },
+  };
+};
+
+export default generateSEOData;
