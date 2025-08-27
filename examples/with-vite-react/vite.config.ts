71b9db06d91a7b5e3832eb90f776d402e8febccb
refirst11 <ocelot2828@gmail.com>
2025-08-27 14:43:49 +0900

    examples(deps): update

--- examples/with-vite-react/vite.config.ts
+++ examples/with-vite-react/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import plumeria from '@plumeria/vite-plugin';
+import { plumeria } from '@plumeria/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
