179ba399f76442eabb4c228db98ecd9a5bcdb95d
refirst11 <ocelot2828@gmail.com>
2025-08-30 00:39:05 +0900

    feat(with-astro-react): update

--- examples/with-astro-react/astro.config.mjs
+++ examples/with-astro-react/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { plumeria } from '@plumeria/vite-plugin';
+import react from '@astrojs/react';
+import plumeria from '@plumeria/vite-plugin';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [plumeria()],
   },
