d4e1bd388a4a644b5ba0b34c1ad7a1cd665a0fd3
refirst11 <ocelot2828@gmail.com>
2025-08-30 00:49:52 +0900

    feat(examples): update @plumeria version@0.17.0

--- examples/with-vite-react/src/App.tsx
+++ examples/with-vite-react/src/App.tsx
@@ -8,7 +8,7 @@ const styles = css.create({
   color: {
     WebkitTextFillColor: 'transparent',
     background: 'linear-gradient(90deg, #58c6ff 0%, #3e5875 50%, #ff3bef 100%)',
-    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
   },
 });
 
--- examples/with-vite-vue/vite.config.ts
+++ examples/with-vite-vue/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { plumeria } from '@plumeria/vite-plugin';
+import plumeria from '@plumeria/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
