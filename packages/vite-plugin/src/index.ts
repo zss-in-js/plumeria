b3e8ab49de16b95a28c4111042a10668a88a1a06
refirst11 <ocelot2828@gmail.com>
2025-09-11 10:39:20 +0900

    fix(vite-plugin): plugin name is vite to vite-plugin

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -2,7 +2,7 @@ import type { PluginOption } from 'vite';
 
 export default function plumeria(): PluginOption {
   return {
-    name: '@plumeria/vite',
+    name: '@plumeria/vite-plugin',
     config: ({ build = {} }) => ({
       build: {
         ...build,
