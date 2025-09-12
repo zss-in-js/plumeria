242565f7c74f4f817e0a5822aedbfae638c68797
refirst11 <ocelot2828@gmail.com>
2025-09-12 20:31:50 +0900

    fix(style-not-conflict.tsx): remove utils.mini

--- test-e2e/site/component/style-not-conflict.tsx
+++ test-e2e/site/component/style-not-conflict.tsx
@@ -1,10 +1,8 @@
 import { css } from '@plumeria/core';
-import { utils } from 'component/vars';
 
 const styles = css.create({
   test_conflict: {
     fontSize: 14,
-    lineHeight: utils.mini,
     color: 'pink',
   },
 });
