87cd0e5214fc11c4a8d48b8816be17f617fe95d0
refirst11 <ocelot2828@gmail.com>
2025-09-12 23:10:05 +0900

    docs(global.css): change global api to global.css

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import { baseOptions } from 'app/layout.config';
 import { source } from 'lib/source';
 import '@plumeria/core/stylesheet.css';
-import '../global';
+import '../global.css';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
