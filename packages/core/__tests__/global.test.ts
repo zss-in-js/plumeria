be44637d2afa106d83f93cf2bf268a3ecec0ca52
refirst11 <ocelot2828@gmail.com>
2025-09-12 19:46:20 +0900

    fix(global.test.ts): has been removed api

--- packages/core/__tests__/global.test.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @plumeria/no-inner-call */
-import { css } from '../src/index';
-
-test('global return value is undefined"', () => {
-  const result = css.global({ h1: { fontSize: 24 } });
-  expect(result).toBeUndefined();
-});
