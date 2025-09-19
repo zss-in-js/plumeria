70ee25e7c9dcff59e98a1a47f2afcad75c47d426
refirst11 <ocelot2828@gmail.com>
2025-09-19 21:41:02 +0900

    feat(core/tsdown.config.ts): add checker directory

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -5,14 +5,14 @@ export default defineConfig([
     clean: true,
     dts: true,
     format: ['cjs'],
-    entry: ['src/index.ts', 'src/processors/css.ts'],
+    entry: ['src/index.ts', 'src/processors/css.ts', 'src/checker/index.ts'],
     hash: false,
   },
   {
     clean: true,
     dts: false,
     format: ['esm'],
-    entry: ['src/index.ts', 'src/processors/css.ts'],
+    entry: ['src/index.ts', 'src/processors/css.ts', 'src/checker/index.ts'],
     hash: false,
   },
 ]);
