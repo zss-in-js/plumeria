c22d1865a3a37ab3908a35909c03e3ef9f8750e1
refirst11 <ocelot2828@gmail.com>
2025-09-23 11:15:37 +0900

    feat(webpack-plugin/types.ts): update add tokens types

--- packages/webpack-plugin/src/types.ts
+++ packages/webpack-plugin/src/types.ts
@@ -18,8 +18,7 @@ export type CSSObject = {
 // Table types for various CSS constructs
 export type ConstTable = Record<string, CSSObject | string>; // varName -> object
 export type VariableTable = Record<string, CSSObject>; // varName -> object
-export type ThemeTable = Record<string, CSSObject>; // varName -> object
+export type TokensTable = Record<string, CSSObject>; // varName -> object
 export type KeyframesHashTable = Record<string, string>; // varName -> hash
 export type KeyframesObjectTable = Record<string, CSSObject>; // hashKey: { string: { string: string | number }, ... }
-export type DefineVarsObjectTable = Record<string, CSSObject>; // { string: { varName: value }, ... }
-export type DefineThemeObjectTable = Record<string, CSSObject>; // { string: { varName: { varName: value }, ... } }
+export type DefineTokensObjectTable = Record<string, CSSObject>; // { string: { varName: { varName: value }, ... } }
