f99ede577d2f193610e588cbe48e987dcc6821d7
refirst11 <ocelot2828@gmail.com>
2025-09-28 03:23:18 +0900

    feat(eslint-plugin): index.ts add types

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -3,6 +3,7 @@ import { noInnerCall } from './rules/no-inner-call.js';
 import { noUnusedKeys } from './rules/no-unused-keys';
 import { sortProperties } from './rules/sort-properties';
 import { validateValues } from './rules/validate-values';
+import { ESLint, Linter, Rule } from 'eslint';
 
 const rules = {
   'no-destructure': noDestructure,
@@ -42,8 +43,22 @@ const flatConfigs = {
   },
 };
 
-module.exports = {
+export const plumeria = {
   rules,
   configs,
   flatConfigs,
+} as unknown as ESLint.Plugin & {
+  rules: {
+    'no-destructure': Rule.RuleModule;
+    'no-inner-call': Rule.RuleModule;
+    'no-unused-keys': Rule.RuleModule;
+    'sort-properties': Rule.RuleModule;
+    'validate-values': Rule.RuleModule;
+  };
+  configs: {
+    recommended: Linter.LegacyConfig;
+  };
+  flatConfigs: {
+    recommended: Linter.Config<Linter.RulesRecord>;
+  };
 };
