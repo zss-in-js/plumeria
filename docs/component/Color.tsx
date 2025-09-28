f9da4e4f04c91b2b2c9d89d9565f3443b5f19277
refirst11 <ocelot2828@gmail.com>
2025-09-28 22:17:24 +0900

    feat(docs/component): Color.tsx scale updated

--- docs/component/Color.tsx
+++ docs/component/Color.tsx
@@ -3,13 +3,17 @@ import { ps } from 'lib/pseudos';
 
 const styles = css.create({
   colorName: {
+    position: 'relative',
+    top: 4,
     display: 'inline-block',
-    width: 12,
-    height: 12,
+    width: 18,
+    height: 18,
+    cursor: 'pointer',
     background: 'var(--color)',
-    border: 'solid 0.4px black',
+    border: 'solid 0.4px rgba(0,0,0,0.2)',
+    transition: '0.2s',
     [ps.hover]: {
-      scale: 1.5,
+      scale: 1.75,
     },
   },
 });
