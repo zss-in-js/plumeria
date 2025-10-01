afc80c4473b9cefd27233781129cfd19129cec0b
refirst11 <ocelot2828@gmail.com>
2025-10-02 00:55:51 +0900

    feat(docs/app/(home)/page.tsx): update title and subtitle

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -2,7 +2,10 @@ import { HomeComponent } from 'component/HomeComponent';
 import { Metadata } from 'next';
 import generateSEOData from 'lib/generateSEOData';
 
-export const metadata: Metadata = generateSEOData({ title: '', subtitle: 'Zero-runtime CSS in JS library' });
+export const metadata: Metadata = generateSEOData({
+  title: 'Plumeria - Atomic CSS in JS library',
+  subtitle: 'The atomic on-demand CSS-in-JS.',
+});
 
 export default function Page() {
   return <HomeComponent />;
