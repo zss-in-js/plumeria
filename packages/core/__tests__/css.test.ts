2e8ede4f50f1f88d1fcd8ab68b080adbe0c18e1c
refirst11 <ocelot2828@gmail.com>
2025-10-09 20:58:48 +0900

    chore(core/__tests__): add for coverage

---
+++ packages/core/__tests__/css.test.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @plumeria/no-inner-call */
+import { css, rx } from '../src/css';
+
+describe('css static methods', () => {
+  it('delegates keyframes()', () => {
+    const res = css.keyframes({
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+    });
+    expect(typeof res).toBe('string');
+  });
+
+  it('delegates viewTransition()', () => {
+    const res = css.viewTransition({ new: { opacity: 0 } });
+    expect(typeof res).toBe('string');
+  });
+
+  it('delegates defineConsts()', () => {
+    const res = css.defineConsts({ primary: 'blue' });
+    expect(res.primary).toBe('blue');
+  });
+
+  it('delegates defineTokens()', () => {
+    const res = css.defineTokens({
+      color: { primary: '#fff' },
+    });
+    expect(res.color.startsWith('var')).toBe(true);
+  });
+
+  it('delegates create() and props()', () => {
+    const res = css.create({
+      text: {
+        fontSize: 24,
+      },
+    });
+    const className = css.props(res.text);
+    expect(typeof res.text).toBe('object');
+    expect(typeof className).toBe('string');
+  });
+});
+
+describe('rx', () => {
+  it('should return an object with the given className and varSet as style', () => {
+    const className = 'my-class';
+    const varSet = {
+      '--primary-color': 'blue',
+      '--font-weight': 'bold',
+    };
+
+    const result = rx(className, varSet);
+
+    expect(result).toEqual({
+      className: className,
+      style: varSet,
+    });
+  });
+});
---
+++ packages/core/__tests__/index.test.ts
@@ -0,0 +1,10 @@
+import { css, rx } from '../src/index';
+
+describe('css rx', () => {
+  it('should be defined', () => {
+    expect(css).toBeDefined();
+  });
+  it('should be defined', () => {
+    expect(rx).toBeDefined();
+  });
+});
