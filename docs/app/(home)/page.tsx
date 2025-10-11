6a7eed857698400b469d17afa59cd02738127ea5
refirst11 <ocelot2828@gmail.com>
2025-10-11 20:25:32 +0900

    feat(docs/(home)/page.tsx): update meta description

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -4,7 +4,8 @@ import generateSEOData from 'lib/generateSEOData';
 
 export const metadata: Metadata = generateSEOData({
   title: 'Plumeria - Atomic CSS in JS library',
-  subtitle: 'The atomic on-demand CSS-in-JS.',
+  subtitle:
+    'The atomic on-demand CSS-in-JS. Plumeria compiles at build time, generating only the styles you use for optimal performance and size.',
 });
 
 export default function Page() {
