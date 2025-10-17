b2581281dece67944efaf51a55bb5aca5b528c9d
refirst11 <ocelot2828@gmail.com>
2025-10-17 20:59:38 +0900

    feat(jest.config.js): add next-plugin and webpack-plugin tests

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,17 @@ module.exports = {
   roots: [
     '<rootDir>/packages/core/__tests__',
     '<rootDir>/packages/eslint-plugin/__tests__',
+    '<rootDir>/packages/next-plugin/__tests__',
+    '<rootDir>/packages/webpack-plugin/__tests__',
   ],
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageReporters: ['text', 'lcov', 'html'],
   reporters: [['github-actions', { silent: false }], 'summary'],
-  coveragePathIgnorePatterns: ['<rootDir>/packages/core/dist/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/packages/core/dist/',
+    '<rootDir>/packages/eslint-plugin/dist/',
+    '<rootDir>/packages/next-plugin/dist/',
+    '<rootDir>/packages/webpack-plugin/dist/',
+  ],
 };
