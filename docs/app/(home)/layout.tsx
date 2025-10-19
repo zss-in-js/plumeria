166704e863f714524bb52459eb7a7f4de5718ebc
refirst11 <ocelot2828@gmail.com>
2025-10-19 18:26:09 +0900

    chore(docs/(home)/layout.tsx): sorted import css

--- docs/app/(home)/layout.tsx
+++ docs/app/(home)/layout.tsx
@@ -1,8 +1,8 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from 'app/layout.config';
-import '@plumeria/core/stylesheet.css';
 import '../global.css';
+import '@plumeria/core/stylesheet.css';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
