b68439eedea9051b2329e3e6256d61bed8d3bdc3
refirst11 <ocelot2828@gmail.com>
2025-11-04 11:50:36 +0900

    refactor(core/create.ts): moved overrideLonghand inside zss-engine

--- packages/core/src/api/create.ts
+++ packages/core/src/api/create.ts
@@ -5,9 +5,7 @@ import type {
   ReturnType,
 } from 'zss-engine';
 import {
-  SHORTHAND_PROPERTIES,
-  LONG_TO_SHORT,
-  camelToKebabCase,
+  overrideLonghand,
   splitAtomicAndNested,
   processAtomicProps,
   genBase36Hash,
@@ -33,58 +31,6 @@ function create<const T extends Record<string, CSSProperties>>(
     const nonFlat: CreateStyle = {};
 
     splitAtomicAndNested(styleObj, flat, nonFlat);
-
-    const overrideLonghand = (style: CreateStyle) => {
-      const props = Object.keys(style);
-      const propsToRemove = new Set<string>();
-
-      // Find plain props and their indices
-      const plainProps: { key: string; index: number }[] = [];
-      for (let i = 0; i < props.length; i++) {
-        if (!props[i].startsWith('@')) {
-          plainProps.push({ key: props[i], index: i });
-        }
-      }
-
-      // Find shorthands among plain props
-      const shorthandsInStyle: { [key: string]: number } = {};
-      for (const { key, index } of plainProps) {
-        const kebab = camelToKebabCase(key);
-        if (SHORTHAND_PROPERTIES[kebab]) {
-          shorthandsInStyle[kebab] = index;
-        }
-      }
-
-      // Determine which longhands to remove
-      for (const { key, index } of plainProps) {
-        const kebab = camelToKebabCase(key);
-        if (!SHORTHAND_PROPERTIES[kebab]) {
-          // is longhand
-          const longhands = LONG_TO_SHORT[kebab] || [];
-          for (const shorthand of longhands) {
-            if (shorthandsInStyle[shorthand] > index) {
-              propsToRemove.add(key);
-              break;
-            }
-          }
-        }
-      }
-
-      const finalStyle: CreateStyle = {};
-      for (const prop of props) {
-        if (propsToRemove.has(prop)) {
-          continue;
-        }
-        if (prop.startsWith('@')) {
-          finalStyle[prop] = overrideLonghand(style[prop] as CreateStyle);
-        } else {
-          finalStyle[prop] = style[prop];
-        }
-      }
-
-      return finalStyle;
-    };
-
     const finalFlat = overrideLonghand(flat);
 
     const records: Array<{
