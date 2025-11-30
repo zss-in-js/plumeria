b458771f478a1ccb395147f9e34d74f9d9ae8a2c
refirst11 <ocelot2828@gmail.com>
2025-11-30 16:51:08 +0900

    feat(core/x.ts): API rename and update

--- packages/core/src/api/x.ts
+++ packages/core/src/api/x.ts
@@ -1,10 +1,10 @@
 import type { RxVariableSet } from 'zss-engine';
 
-const rx = (className: string, varSet: RxVariableSet) => ({
+const x = (className: string, varSet: RxVariableSet) => ({
   className,
   style: Object.fromEntries(
     Object.entries(varSet).map(([key, value]) => [key, value]),
   ),
 });
 
-export { rx };
+export { x };
