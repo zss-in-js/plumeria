f0977b603561e73df21e96f482c5c1b4e4ca46b9
refirst11 <ocelot2828@gmail.com>
2025-12-02 21:19:33 +0900

    docs(animation.ts): add

---
+++ docs/component/animation.ts
@@ -0,0 +1,7 @@
+import { css } from '@plumeria/core';
+
+export const gradientShift = css.keyframes({
+  '0%': { backgroundPosition: '0% 50%' },
+  '50%': { backgroundPosition: '100% 50%' },
+  '100%': { backgroundPosition: '0% 50%' },
+});
