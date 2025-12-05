47b87b12749b46f3e4baf6aae00cb6949a522fbd
refirst11 <ocelot2828@gmail.com>
2025-12-05 20:19:45 +0900

    chore(docs/CodeBlock.tsx): adjust ui

--- docs/component/CodeBlock.tsx
+++ docs/component/CodeBlock.tsx
@@ -5,20 +5,17 @@ import { breakpoints } from 'lib/mediaQuery';
 const stylesCode = css.create({
   code_div: {
     position: 'relative',
-    right: 46,
-    bottom: 8,
     zIndex: 0,
     display: 'flex',
     width: '100%',
-    maxWidth: '520px',
-    padding: '20px 28px',
+    maxWidth: '600px',
+    padding: '20px 24px',
     overflow: 'hidden',
     fontFamily: 'var(--font-geist-mono)',
     fontSize: 13,
     borderRadius: '12px',
     backdropFilter: 'blur(12px)',
     [breakpoints.md]: {
-      right: 0,
       maxWidth: '340px',
       padding: '20px 24px',
       overflowX: 'scroll',
