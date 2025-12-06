544cdddaa83c831618ff902b8d7e76b3ea90ea5e
refirst11 <ocelot2828@gmail.com>
2025-12-06 13:00:02 +0900

    chore(astro.config.mjs): Increase the priority of the plugin

--- examples/with-astro-react/astro.config.mjs
+++ examples/with-astro-react/astro.config.mjs
@@ -2,12 +2,12 @@
 // @ts-nocheck
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import plumeria from '@plumeria/vite-plugin';
+import { plumeria } from '@plumeria/vite-plugin';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   vite: {
     plugins: [plumeria()],
   },
+  integrations: [react()],
 });
