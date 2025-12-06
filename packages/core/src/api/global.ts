ce848d349cf4358b2c16ed29435c494e32c7e590
refirst11 <ocelot2828@gmail.com>
2025-12-06 14:28:28 +0900

    chore(core): global.ts removed sheet injection

--- packages/core/src/api/global.ts
+++ packages/core/src/api/global.ts
@@ -1,10 +1,5 @@
 import type { CSSHTML } from 'zss-engine';
-import {
-  transpile,
-  isServer,
-  isTestingDevelopment,
-  injectClientGlobalCSS,
-} from 'zss-engine';
+import { transpile } from 'zss-engine';
 import {
   initPromise_2,
   globalPromise_2,
@@ -16,8 +11,6 @@ function global(object: CSSHTML): void {
 
   if (typeof globalPromise_2 === 'undefined') initPromise_2();
   resolvePromise_2(styleSheet);
-
-  if (isTestingDevelopment && !isServer) injectClientGlobalCSS(styleSheet);
 }
 
 export { global };
