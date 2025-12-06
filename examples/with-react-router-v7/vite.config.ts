a44f2aac034611c7b65445b801c5a6f8eb19a15c
refirst11 <ocelot2828@gmail.com>
2025-12-06 13:08:11 +0900

    chore(react-router): vite.config.ts import fix

--- examples/with-react-router-v7/vite.config.ts
+++ examples/with-react-router-v7/vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import plumeria from '@plumeria/vite-plugin';
+import { plumeria } from '@plumeria/vite-plugin';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), plumeria()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    plumeria() as Plugin,
+  ],
 });
