53f0090b32916826d4cfb659335428cda6cf0a6a
refirst11 <ocelot2828@gmail.com>
2025-12-06 13:00:34 +0900

    chore(svelte/vite.config.ts)Increase the priority of the plugin

--- examples/with-vite-svelte/vite.config.ts
+++ examples/with-vite-svelte/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import plumeria from '@plumeria/vite-plugin';
+import { plumeria } from '@plumeria/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), plumeria()],
+  plugins: [plumeria(), svelte()],
 });
