2c76cc599c0ca5f8236570e347c223c604ebfbed
refirst11 <ocelot2828@gmail.com>
2025-12-11 19:55:17 +0900

    chore(docs): add svg

---
+++ docs/component/svg.tsx
@@ -0,0 +1,170 @@
+export const svg = {
+  TurbopackIcon() {
+    /*
+     * Author: Gil Barbara
+     * License: CC0
+     */
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 256 293">
+        <title>Turbopack-icon SVG Icon</title>
+        <defs>
+          <linearGradient id="logosTurbopackIcon0" x1="50%" x2="49.855%" y1="7.896%" y2="93.343%">
+            <stop offset="0%" stopColor="#0096FF"></stop>
+            <stop offset="100%" stopColor="#FF1E56"></stop>
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#logosTurbopackIcon0)"
+          d="M128 0L46.16 46.768l.16 36.752L128 36.848l96 54.864L256 110V73.136zm-14.4 247.488L32 200.864V91.792l-.032-.016l-.176-36.8L0 73.136v146.288l81.68 46.672zM224 200.864v-92.56l32 18.288v92.832l-128 73.152l-31.888-18.224L128 255.76v-.048z"
+        ></path>
+        <path d="M46.4 108.369v84.128l81.6 46.64v-84.144zM128 53.441l-74.352 42.48l81.616 46.64l74.336-42.496z"></path>
+      </svg>
+    );
+  },
+
+  Atom() {
+    /*
+     * Author: Marketeq
+     * License: MIT
+     */
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 50 50">
+        <title>Atom SVG Icon</title>
+        <g fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="#344054" strokeWidth="3" d="M24.896 25h.208"></path>
+          <path
+            stroke="#306CFE"
+            strokeWidth="2"
+            d="M33.333 25c0 10.417-3.729 18.75-8.333 18.75S16.667 35.417 16.667 25S20.396 6.25 25 6.25s8.333 8.333 8.333 18.75M8.854 34.896c2.292 4.166 11.375 3.208 20.292-2.271c8.917-5.48 14.27-13.312 12-17.52c-2.271-4.21-11.375-3.21-20.313 2.27S6.583 30.688 8.854 34.895m32.292 0c2.27-4.167-3.084-12.042-12-17.521s-18-6.5-20.292-2.27c-2.291 4.228 3.084 11.978 11.98 17.52c8.895 5.542 18.02 6.5 20.312 2.27"
+          ></path>
+        </g>
+      </svg>
+    );
+  },
+
+  Vscode() {
+    /*
+     * Author: konpa
+     * License: MIT
+     */
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 128 128">
+        <title>Vscode SVG Icon</title>
+        <mask id="deviconVscode0" width="128" height="128" x="0" y="0" maskUnits="userSpaceOnUse">
+          <path
+            fill="#fff"
+            fillRule="evenodd"
+            d="M90.767 127.126a7.97 7.97 0 0 0 6.35-.244l26.353-12.681a8 8 0 0 0 4.53-7.209V21.009a8 8 0 0 0-4.53-7.21L97.117 1.12a7.97 7.97 0 0 0-9.093 1.548l-50.45 46.026L15.6 32.013a5.33 5.33 0 0 0-6.807.302l-7.048 6.411a5.335 5.335 0 0 0-.006 7.888L20.796 64L1.74 81.387a5.336 5.336 0 0 0 .006 7.887l7.048 6.411a5.33 5.33 0 0 0 6.807.303l21.974-16.68l50.45 46.025a8 8 0 0 0 2.743 1.793Zm5.252-92.183L57.74 64l38.28 29.058V34.943Z"
+            clipRule="evenodd"
+          ></path>
+        </mask>
+        <g mask="url(#deviconVscode0)">
+          <path
+            fill="#0065A9"
+            d="M123.471 13.82L97.097 1.12A7.97 7.97 0 0 0 88 2.668L1.662 81.387a5.333 5.333 0 0 0 .006 7.887l7.052 6.411a5.33 5.33 0 0 0 6.811.303l103.971-78.875c3.488-2.646 8.498-.158 8.498 4.22v-.306a8 8 0 0 0-4.529-7.208Z"
+          ></path>
+          <g filter="url(#deviconVscode1)">
+            <path
+              fill="#007ACC"
+              d="m123.471 114.181l-26.374 12.698A7.97 7.97 0 0 1 88 125.333L1.662 46.613a5.333 5.333 0 0 1 .006-7.887l7.052-6.411a5.33 5.33 0 0 1 6.811-.303l103.971 78.874c3.488 2.647 8.498.159 8.498-4.219v.306a8 8 0 0 1-4.529 7.208"
+            ></path>
+          </g>
+          <g filter="url(#deviconVscode2)">
+            <path
+              fill="#1F9CF0"
+              d="M97.098 126.882A7.98 7.98 0 0 1 88 125.333c2.952 2.952 8 .861 8-3.314V5.98c0-4.175-5.048-6.266-8-3.313a7.98 7.98 0 0 1 9.098-1.549L123.467 13.8A8 8 0 0 1 128 21.01v85.982a8 8 0 0 1-4.533 7.21z"
+            ></path>
+          </g>
+          <path
+            fill="url(#deviconVscode3)"
+            fillRule="evenodd"
+            d="M90.69 127.126a7.97 7.97 0 0 0 6.349-.244l26.353-12.681a8 8 0 0 0 4.53-7.21V21.009a8 8 0 0 0-4.53-7.21L97.039 1.12a7.97 7.97 0 0 0-9.093 1.548l-50.45 46.026l-21.974-16.68a5.33 5.33 0 0 0-6.807.302l-7.048 6.411a5.336 5.336 0 0 0-.006 7.888L20.718 64L1.662 81.386a5.335 5.335 0 0 0 .006 7.888l7.048 6.411a5.33 5.33 0 0 0 6.807.303l21.975-16.681l50.45 46.026a8 8 0 0 0 2.742 1.793m5.252-92.184L57.662 64l38.28 29.057z"
+            clipRule="evenodd"
+            opacity=".25"
+          ></path>
+        </g>
+        <defs>
+          <filter
+            id="deviconVscode1"
+            width="144.744"
+            height="113.408"
+            x="-8.411"
+            y="22.594"
+            colorInterpolationFilters="sRGB"
+            filterUnits="userSpaceOnUse"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+            <feColorMatrix
+              in="SourceAlpha"
+              result="hardAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            ></feColorMatrix>
+            <feOffset></feOffset>
+            <feGaussianBlur stdDeviation="4.167"></feGaussianBlur>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix>
+            <feBlend in2="BackgroundImageFix" mode="overlay" result="effect1_dropShadow_1_36"></feBlend>
+            <feBlend in="SourceGraphic" in2="effect1_dropShadow_1_36" result="shape"></feBlend>
+          </filter>
+          <filter
+            id="deviconVscode2"
+            width="56.667"
+            height="144.007"
+            x="79.667"
+            y="-8.004"
+            colorInterpolationFilters="sRGB"
+            filterUnits="userSpaceOnUse"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+            <feColorMatrix
+              in="SourceAlpha"
+              result="hardAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            ></feColorMatrix>
+            <feOffset></feOffset>
+            <feGaussianBlur stdDeviation="4.167"></feGaussianBlur>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix>
+            <feBlend in2="BackgroundImageFix" mode="overlay" result="effect1_dropShadow_1_36"></feBlend>
+            <feBlend in="SourceGraphic" in2="effect1_dropShadow_1_36" result="shape"></feBlend>
+          </filter>
+          <linearGradient
+            id="deviconVscode3"
+            x1="63.922"
+            x2="63.922"
+            y1=".33"
+            y2="127.67"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#fff"></stop>
+            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+    );
+  },
+
+  Feather() {
+    /*
+     * Author: Microsoft Corporation
+     * License: MIT
+     */
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 32 32">
+        <title>Feather SVG Icon</title>
+        <g fill="none">
+          <path
+            fill="#F3AD61"
+            d="M29.374 2h-4.17c-2.112 0-4.195.299-6.193.874l-1.992 3.17L14.013 5a22.395 22.395 0 0 0-3.269 2.3a19.025 19.025 0 0 0-6.72 14.51v3.25s.17-.3.49-.81c-.09.38-.13.59-.13.59v.22h3.92c2.08 0 4.08-.47 5.89-1.34c.21-.1.21-.4 0-.5l-2.17-1.01c-.13-.04-.09-.21.04-.21h4.53c.21 0 .41-.08.57-.21c1.33-1.14 2.45-2.54 3.27-4.15c.3-.6.87-1.67 1.18-2.25c.13-.24.12-.53-.02-.77l-1.36-2.24a.25.25 0 0 1 .21-.38l1.533-.994l2.044-1.998l2.76-2.002a45.861 45.861 0 0 1 3.043-3.816c.13-.14.2-.33.2-.52a.653.653 0 0 0-.65-.67"
+          ></path>
+          <path
+            fill="#A56953"
+            d="M14 12h9.114c.26 0 .51-.14.64-.36c.94-1.57 1.96-3.14 3.04-4.64h-7.79V2.88A22.129 22.129 0 0 0 14 5.016z"
+          ></path>
+          <path
+            fill="#D3883E"
+            d="M25.284 4.14a.498.498 0 0 0-.65-.23c-4.6 2.07-8.74 4.94-12.34 8.53a41.584 41.584 0 0 0-8.92 13.22a41.47 41.47 0 0 0-1.35 3.67c-.09.3.08.61.38.66c.26.05.5-.11.57-.35c.38-1.21.82-2.41 1.32-3.59c2.04-4.83 4.97-9.18 8.7-12.9c3.51-3.51 7.55-6.3 12.04-8.32a.5.5 0 0 0 .25-.69c0 .01 0 .01 0 0"
+          ></path>
+        </g>
+      </svg>
+    );
+  },
+};
