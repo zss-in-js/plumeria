c295f31ca31b134136427e2e3c993574a81ee67c
refirst11 <ocelot2828@gmail.com>
2025-12-12 16:57:50 +0900

    chore(utils/transform.ts): Fixed due to API changes

--- packages/utils/src/transform.ts
+++ packages/utils/src/transform.ts
@@ -1,7 +1,7 @@
 import type {
   CSSProperties,
   CreateStyleType,
-  CreateTokens,
+  CreateTheme,
   CreateValues,
   CreateStyle,
 } from 'zss-engine';
@@ -10,7 +10,6 @@ import {
   processAtomicProps,
   genBase36Hash,
   transpile,
-  transformNestedSelectors,
   camelToKebabCase,
   overrideLonghand,
 } from 'zss-engine';
@@ -81,25 +80,39 @@ function compileToSingleCSS<T extends Record<string, CSSProperties>>(
     });
 
     if (Object.keys(nonFlat).length > 0) {
-      const modNonFlat = transformNestedSelectors(nonFlat);
-      const nonFlatObj = { [key]: modNonFlat };
-      const nonFlatHash = genBase36Hash(nonFlatObj, 1, 7);
-      const { styleSheet } = transpile(nonFlatObj, nonFlatHash);
-      const isQuery =
-        styleSheet.includes('@media') || styleSheet.includes('@container');
-      const finalSheet = isQuery
-        ? styleSheet.replace(
-            `.${nonFlatHash}`,
-            `.${nonFlatHash}:not(#\\#):not(#\\#)`,
-          )
-        : styleSheet;
+      const nonFlatBase: CreateStyle = {};
+      const nonFlatQuery: CreateStyle = {};
 
       Object.entries(nonFlat).forEach(([atRule, nestedObj]) => {
-        Object.keys(nestedObj).forEach((prop) => {
-          records.push({
-            key: atRule + prop,
-            hash: nonFlatHash,
-            sheet: finalSheet,
+        if (atRule.startsWith('@media') || atRule.startsWith('@container')) {
+          nonFlatQuery[atRule] = nestedObj;
+        } else {
+          nonFlatBase[atRule] = nestedObj;
+        }
+      });
+
+      [nonFlatBase, nonFlatQuery].forEach((targetNonFlat) => {
+        if (Object.keys(targetNonFlat).length === 0) return;
+
+        const nonFlatObj = { [key]: targetNonFlat };
+        const nonFlatHash = genBase36Hash(nonFlatObj, 1, 7);
+        const { styleSheet } = transpile(nonFlatObj, nonFlatHash);
+        const isQuery =
+          styleSheet.includes('@media') || styleSheet.includes('@container');
+        const finalSheet = isQuery
+          ? styleSheet.replace(
+              `.${nonFlatHash}`,
+              `.${nonFlatHash}:not(#\\#):not(#\\#)`,
+            )
+          : styleSheet;
+
+        Object.entries(targetNonFlat).forEach(([atRule, nestedObj]) => {
+          Object.keys(nestedObj).forEach((prop) => {
+            records.push({
+              key: atRule + prop,
+              hash: nonFlatHash,
+              sheet: finalSheet,
+            });
           });
         });
       });
@@ -140,7 +153,7 @@ const createVars = <const T extends CreateValues>(object: T) => {
   return styles;
 };
 
-const createTokens = <const T extends CreateTokens>(object: T) => {
+const createTheme = <const T extends CreateTheme>(object: T) => {
   const styles: Record<string, Record<string, string | number | object>> = {};
 
   Object.entries(object).forEach(([key, value]) => {
@@ -164,4 +177,4 @@ const createTokens = <const T extends CreateTokens>(object: T) => {
   return styles;
 };
 
-export { createCSS, createVars, createTokens };
+export { createCSS, createVars, createTheme };
