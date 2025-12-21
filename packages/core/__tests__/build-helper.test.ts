e66aeb84c25a86a0bc3bf6f171b84d2a4f7df55c
refirst11 <ocelot2828@gmail.com>
2025-12-21 15:56:12 +0900

    chore(core/__tests__/build-helper.test): update

--- packages/core/__tests__/build-helper.test.ts
+++ packages/core/__tests__/build-helper.test.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @plumeria/no-inner-call */
-import { globalPromise_1 } from '../src/processors/css';
+import { pQueue } from '../src/processors/css';
 import { css } from '../src/index';
 
-test('set function should create globalPromise and add styles to it', () => {
-  expect(globalPromise_1).toBeUndefined();
+test('set function should create pQueue.promise and add styles to it', () => {
+  expect(pQueue.promise).toBeUndefined();
   const styles = css.create({ test: { color: 'red' } });
   css.props(styles.test);
-  expect(globalPromise_1).toBeDefined();
-  expect(globalPromise_1).resolves.toContain('color: red');
+  expect(pQueue.promise).toBeDefined();
+  expect(pQueue.promise).resolves.toContain('color: red');
 });
