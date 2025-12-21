0b32f2c5a4ef5803a03e90c8e73ffc3601dc91df
refirst11 <ocelot2828@gmail.com>
2025-12-21 15:24:54 +0900

    refactor(core/global.ts): processors to use Queue object

--- packages/core/src/api/global.ts
+++ packages/core/src/api/global.ts
@@ -1,16 +1,12 @@
 import type { CSSHTML } from 'zss-engine';
 import { transpile } from 'zss-engine';
-import {
-  initPromise_2,
-  globalPromise_2,
-  resolvePromise_2,
-} from '../processors/css';
+import { gQueue } from '../processors/css';
 
 function global(object: CSSHTML): void {
   const { styleSheet } = transpile(object, undefined, '--global');
 
-  if (typeof globalPromise_2 === 'undefined') initPromise_2();
-  resolvePromise_2(styleSheet);
+  if (typeof gQueue.promise === 'undefined') gQueue.init();
+  gQueue.resolve(styleSheet);
 }
 
 export { global };
