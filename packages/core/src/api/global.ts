fa14d7cc553c20cf2c983ae0b8e7e1240ba4010e
refirst11 <ocelot2828@gmail.com>
2025-12-23 18:07:35 +0900

    refactor(core/global.ts): object changed to rule

--- packages/core/src/api/global.ts
+++ packages/core/src/api/global.ts
@@ -2,8 +2,8 @@ import type { CSSHTML } from 'zss-engine';
 import { transpile } from 'zss-engine';
 import { gQueue } from '../processors/css';
 
-function global(object: CSSHTML): void {
-  const { styleSheet } = transpile(object, undefined, '--global');
+function global(rule: CSSHTML): void {
+  const { styleSheet } = transpile(rule, undefined, '--global');
 
   if (typeof gQueue.promise === 'undefined') gQueue.init();
   gQueue.resolve(styleSheet);
