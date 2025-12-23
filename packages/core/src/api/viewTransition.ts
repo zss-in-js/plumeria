1e37c83124502d05fac3169f40348a0ca5411cf8
refirst11 <ocelot2828@gmail.com>
2025-12-23 18:08:05 +0900

    refactor(core/viewTransition.ts): object changed to rule

--- packages/core/src/api/viewTransition.ts
+++ packages/core/src/api/viewTransition.ts
@@ -2,16 +2,15 @@ import type { CSSProperties, ViewTransitionOptions } from 'zss-engine';
 import { genBase36Hash } from 'zss-engine';
 import { global } from './global';
 
-const viewTransition = (object: ViewTransitionOptions): string => {
-  const hash = genBase36Hash(object, 1, 8);
+const viewTransition = (rule: ViewTransitionOptions): string => {
+  const hash = genBase36Hash(rule, 1, 8);
   const ident = `vt-${hash}`;
 
   global({
-    [`::view-transition-group(${ident})`]: object.group as CSSProperties,
-    [`::view-transition-image-pair(${ident})`]:
-      object.imagePair as CSSProperties,
-    [`::view-transition-old(${ident})`]: object.old as CSSProperties,
-    [`::view-transition-new(${ident})`]: object.new as CSSProperties,
+    [`::view-transition-group(${ident})`]: rule.group as CSSProperties,
+    [`::view-transition-image-pair(${ident})`]: rule.imagePair as CSSProperties,
+    [`::view-transition-old(${ident})`]: rule.old as CSSProperties,
+    [`::view-transition-new(${ident})`]: rule.new as CSSProperties,
   });
 
   return ident;
