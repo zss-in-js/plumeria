4d2568ffbb0cdf8d4a67cb998c89f5fac333ce8a
refirst11 <ocelot2828@gmail.com>
2025-12-23 18:07:19 +0900

    refactor(core/createStatic.ts): object changed to rule

--- packages/core/src/api/createStatic.ts
+++ packages/core/src/api/createStatic.ts
@@ -1,7 +1,7 @@
 import type { CreateValues } from 'zss-engine';
 
-const createStatic = <const T extends CreateValues>(object: T): T => {
-  return object;
+const createStatic = <const T extends CreateValues>(rule: T): T => {
+  return rule;
 };
 
 export { createStatic };
