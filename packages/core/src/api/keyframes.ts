71a7561640715d791e7a02b54654e7a8a1221bac
refirst11 <ocelot2828@gmail.com>
2025-12-23 18:07:49 +0900

    refactor(core/keyframes.ts): object changed to rule

--- packages/core/src/api/keyframes.ts
+++ packages/core/src/api/keyframes.ts
@@ -2,10 +2,10 @@ import type { CreateKeyframes } from 'zss-engine';
 import { genBase36Hash } from 'zss-engine';
 import { global } from './global';
 
-const keyframes = (object: CreateKeyframes): string => {
-  const hash = genBase36Hash(object, 1, 8);
+const keyframes = (rule: CreateKeyframes): string => {
+  const hash = genBase36Hash(rule, 1, 8);
   const ident = `kf-${hash}`;
-  global({ [`@keyframes ${ident}`]: object });
+  global({ [`@keyframes ${ident}`]: rule });
   return ident;
 };
 
