11823834a66abe3a5506fd1586129bb2bed6ead2
refirst11 <ocelot2828@gmail.com>
2025-12-26 21:42:37 +0900

    fix(component-class-attach.test): hash is changed 64bit

--- test-e2e/test/component-class-attach.test.ts
+++ test-e2e/test/component-class-attach.test.ts
@@ -4,5 +4,5 @@ test('Components have correct classes applied', async ({ page }) => {
   await page.goto('http://localhost:4000');
   await page.waitForSelector('[data-testid="e2e-test-div"]');
   const div = page.locator('[data-testid="e2e-test-div"]');
-  await expect(div).toHaveClass('x13vu618 x15qdpgc');
+  await expect(div).toHaveClass('x9khpzv9 xjh5z8ql');
 });
