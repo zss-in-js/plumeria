8cc743abc566f41749ff8adee1022f143681a0a1
refirst11 <ocelot2828@gmail.com>
2025-12-28 18:51:14 +0900

    chore(compiler): processors.d.ts removed

--- packages/compiler/processors.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@plumeria/core/processors' {
-  export const gQueue: {
-    build(filePath: string): Promise<void>;
-  };
-  export const pQueue: {
-    build(filePath: string): Promise<void>;
-  };
-}
