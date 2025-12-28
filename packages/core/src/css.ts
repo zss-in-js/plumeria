518c30b350e23b0ca10416138ede5f49f6dc4ddc
refirst11 <ocelot2828@gmail.com>
2025-12-29 00:13:26 +0900

    chore(core): refactor redundant implementation

--- packages/core/src/css.ts
+++ packages/core/src/css.ts
@@ -12,79 +12,40 @@ import type {
 } from './types';
 
 function errorFn(fn: string) {
-  throw new Error(
-    'Using the "css.' +
-      fn +
-      '" is runtime not supported. Make sure you have setup bundler-plugin correctly.',
-  );
+  throw new Error(`css.${fn} requires bundler-plugin setup`);
 }
-
-function create<const T extends Record<string, CSSProperties>>(
-  _rule: CreateStyleType<T>,
-): ReturnType<T> {
-  throw errorFn('create');
-}
-
-function props(...rules: (false | CSSProperties | null | undefined)[]): string {
-  return rules.filter(Boolean).join(' ');
-}
-
-function createStatic<const T extends CreateStatic>(_rule: T): T {
-  throw errorFn('createStatic');
-}
-
-function createTheme<const T extends CreateTheme>(
-  _rule: T,
-): ReturnVariableType<T> {
-  throw errorFn('createTheme');
-}
-
-function keyframes(_rule: Keyframes): string {
-  throw errorFn('keyframes');
-}
-
-function viewTransition(_rule: ViewTransition): string {
-  throw errorFn('viewTransition');
-}
-
-class _css {
+const css = class _css {
   static create<const T extends Record<string, CSSProperties>>(
-    rule: CreateStyleType<T>,
+    _rule: CreateStyleType<T>,
   ): ReturnType<T> {
-    return create(rule);
+    throw errorFn('create');
   }
-
-  static props(
-    ...rules: (false | Readonly<CSSProperties> | null | undefined)[]
-  ): string {
-    return props(...rules);
+  static props(...rules: (false | CSSProperties | null | undefined)[]): string {
+    return rules.filter(Boolean).join(' ');
   }
-
   static createTheme<const T extends CreateTheme>(
-    rule: T,
+    _rule: T,
   ): ReturnVariableType<T> {
-    return createTheme(rule);
+    throw errorFn('createTheme');
   }
 
-  static createStatic<const T extends CreateStatic>(rule: T): T {
-    return createStatic(rule);
+  static createStatic<const T extends CreateStatic>(_rule: T): T {
+    throw errorFn('createStatic');
   }
 
-  static keyframes(rule: Keyframes): string {
-    return keyframes(rule);
+  static keyframes(_rule: Keyframes): string {
+    throw errorFn('keyframes');
   }
 
-  static viewTransition(rule: ViewTransition): string {
-    return viewTransition(rule);
+  static viewTransition(_rule: ViewTransition): string {
+    throw errorFn('viewTransition');
   }
-}
+};
 
 const x = (className: string, styles: Styles) => ({
   className,
   styles,
 });
-
-const css = _css;
 
 export { css, x };
 export type { CreateStyle, CSSProperties };
