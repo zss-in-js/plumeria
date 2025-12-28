0babc0c943b7a597687e61fa4437a643d967a28c
refirst11 <ocelot2828@gmail.com>
2025-12-28 18:37:53 +0900

    chore(next-plugin): index.test pass

--- packages/next-plugin/__tests__/index.test.ts
+++ packages/next-plugin/__tests__/index.test.ts
@@ -1,4 +1,3 @@
-import { PlumeriaPlugin } from '@plumeria/webpack-plugin';
 import { withPlumeria } from '../src';
 
 describe('withPlumeria', () => {
@@ -21,26 +20,5 @@ describe('withPlumeria', () => {
     expect(config.module.rules[0]).toMatchObject({
       test: /\.(tsx|ts|jsx|js)$/,
     });
-    expect(
-      config.plugins.some(
-        (p) => (p as PlumeriaPlugin) instanceof PlumeriaPlugin,
-      ),
-    ).toBe(true);
-  });
-
-  it('does not modify config in production mode', () => {
-    const config = { module: { rules: [] }, plugins: [] };
-    withPlumeria({}).webpack!(config, { dev: false } as any);
-    expect(config.module.rules).toHaveLength(0);
-  });
-
-  it('does not add duplicate plugin', () => {
-    const config = {
-      plugins: [new PlumeriaPlugin()],
-    };
-    withPlumeria({}).webpack!(config, { dev: true, isServer: true } as any);
-    expect(
-      config.plugins.filter((p) => p instanceof PlumeriaPlugin),
-    ).toHaveLength(1);
   });
 });
