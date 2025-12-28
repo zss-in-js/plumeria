ed05a5de121b07261a96d8d0fdbb97c31fafbb12
refirst11 <ocelot2828@gmail.com>
2025-12-28 18:32:38 +0900

    feat(compiler/bin): css.js update

--- packages/compiler/bin/css.js
+++ packages/compiler/bin/css.js
@@ -1,25 +1,4 @@
 #!/usr/bin/env node
-
 const path = require('path');
-const { styleText } = require('util');
-const { enableCompileCache } = require('node:module');
-
-enableCompileCache();
-
-const { register } = require('rscute/register');
-
-register();
-
-try {
-  const stats = process.argv.includes('--stats');
-  const view = process.argv.includes('--view');
-  const paths = process.argv.includes('--paths');
-
-  require(path.resolve(__dirname, '../dist/index.js'));
-
-  if (!stats && !view && !paths)
-    console.log(styleText(['green', 'bold'], '✓ extract...'));
-} catch (error) {
-  console.error('Compilation failed:', error.message);
-  process.exit(1);
-}
+require('rscute/register').register();
+require(path.resolve(__dirname, '../dist/index.js'));
