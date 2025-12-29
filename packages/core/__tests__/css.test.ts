90d79ef4f83bb9911de481e0760e24a204787c12
refirst11 <ocelot2828@gmail.com>
2025-12-29 21:54:02 +0900

    chore(core/__tests__): add props test

--- packages/core/__tests__/css.test.ts
+++ packages/core/__tests__/css.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @plumeria/validate-values */
 /* eslint-disable @plumeria/no-inner-call */
 
 import { css, x } from '../src/css';
@@ -41,5 +42,31 @@ describe('x helper', () => {
       className: 'cls',
       styles: { color: 'red' },
     });
+  });
+});
+
+describe('css.props', () => {
+  test('returns empty string for no args', () => {
+    expect(css.props()).toBe('');
+  });
+
+  test('ignores falsy values', () => {
+    expect(css.props(false, null, undefined, { color: 'a' })).toBe('a');
+  });
+
+  test('different keys preserve left-to-right order', () => {
+    expect(css.props({ color: 'a' }, { background: 'b' })).toBe('a b');
+  });
+
+  test('rightmost key is emitted last', () => {
+    expect(css.props({ color: 'a', background: 'b' }, { color: 'c' })).toBe(
+      'b c',
+    );
+  });
+
+  test('mixed multiple overrides behave correctly', () => {
+    expect(
+      css.props({ color: 'a', margin: 'm1' }, { color: 'b' }, { margin: 'm2' }),
+    ).toBe('b m2');
   });
 });
