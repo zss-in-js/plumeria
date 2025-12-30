859d305e5ded4ad007184026f433d3dce8ab6b34
refirst11 <ocelot2828@gmail.com>
2025-12-30 18:20:50 +0900

    feat(docs): add postcss.config.js

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   plugins: {
     '@plumeria/postcss-plugin': {
       include: '**/*.{ts,tsx}',
-      exclude: ['node_modules/**', '.next/**'],
+      exclude: ['**/node_modules/**', '**/.next/**'],
     },
   },
 };
