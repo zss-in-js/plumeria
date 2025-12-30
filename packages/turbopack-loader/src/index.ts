88e8728928fe6fe96bce3e62773f0b6c0bcb5d54
refirst11 <ocelot2828@gmail.com>
2025-12-30 14:41:32 +0900

    fix(turbopack-loader): keyframes are now compiled when used in viewTransition

--- packages/turbopack-loader/src/index.ts
+++ packages/turbopack-loader/src/index.ts
@@ -248,6 +248,8 @@ export default async function loader(this: LoaderContext, source: string) {
           );
           const hash = genBase36Hash(obj, 1, 8);
           tables.viewTransitionObjectTable[hash] = obj;
+          extractOndemandStyles(obj, extractedSheets);
+          extractOndemandStyles({ vt: `vt-${hash}` }, extractedSheets);
           replacements.push({
             start: node.span.start - ast.span.start,
             end: node.span.end - ast.span.start,
