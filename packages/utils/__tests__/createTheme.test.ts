2d8d687c427eabefcfa07ec93e31fb2359bef9ce
refirst11 <ocelot2828@gmail.com>
2025-12-31 19:28:36 +0900

    chore(utils): update createTheme.test

---
+++ packages/utils/__tests__/createTheme.test.ts
@@ -0,0 +1,39 @@
+import { createTheme } from '../src/createTheme';
+
+describe('createTheme', () => {
+  test('handles default theme', () => {
+    const result = createTheme({
+      color: {
+        default: 'red',
+      },
+    } as any);
+
+    expect(result[':root']).toEqual({
+      '--color': 'red',
+    });
+  });
+
+  test('handles named theme', () => {
+    const result = createTheme({
+      color: {
+        dark: 'black',
+      },
+    } as any);
+
+    expect(result[':root[data-theme="dark"]']).toEqual({
+      '--color': 'black',
+    });
+  });
+
+  test('handles media query', () => {
+    const result = createTheme({
+      color: {
+        '@media (prefers-color-scheme: dark)': 'white',
+      },
+    } as any);
+
+    expect(result[':root']['@media (prefers-color-scheme: dark)']).toEqual({
+      '--color': 'white',
+    });
+  });
+});
