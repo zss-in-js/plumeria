8ab61431aaf9f1c7e56d5706aac545149219cbf8
refirst11 <ocelot2828@gmail.com>
2026-01-04 12:07:48 +0900

    perf(vite-plugin): spread to prototype chain

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -191,26 +191,23 @@ export function plumeria(options: PluginOptions = {}): Plugin {
         },
       });
 
-      const mergedStaticTable = {
-        ...tables.staticTable,
-        ...localConsts,
-        ...importMap,
-      };
-
-      const mergedKeyframesTable = {
-        ...tables.keyframesHashTable,
-        ...importMap,
-      };
-
-      const mergedViewTransitionTable = {
-        ...tables.viewTransitionHashTable,
-        ...importMap,
-      };
-
-      const mergedThemeTable = {
-        ...tables.themeTable,
-        ...importMap,
-      };
+      const mergedStaticTable = Object.assign(
+        Object.create(tables.staticTable),
+        localConsts,
+        importMap,
+      );
+      const mergedKeyframesTable = Object.assign(
+        Object.create(tables.keyframesHashTable),
+        importMap,
+      );
+      const mergedViewTransitionTable = Object.assign(
+        Object.create(tables.viewTransitionHashTable),
+        importMap,
+      );
+      const mergedThemeTable = Object.assign(
+        Object.create(tables.themeTable),
+        importMap,
+      );
 
       const localCreateStyles: Record<
         string,
