6ef558791897c8fce6df62fd7a1a0eca0a4f6c3c
refirst11 <ocelot2828@gmail.com>
2026-01-05 23:25:29 +0900

    chore(core/tsdown.config.ts): has been esm only

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -4,13 +4,6 @@ export default defineConfig([
   {
     clean: true,
     dts: true,
-    format: ['cjs'],
-    entry: ['src/css.ts'],
-    hash: false,
-  },
-  {
-    clean: true,
-    dts: false,
     format: ['esm'],
     entry: ['src/css.ts'],
     hash: false,
