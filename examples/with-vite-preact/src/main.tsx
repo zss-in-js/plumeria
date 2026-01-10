26c23b3e10d3a72f71e3bbbd8bdb0382ff186a44
refirst11 <ocelot2828@gmail.com>
2026-01-10 19:26:59 +0900

    chore(vite-preact): updated

--- examples/with-vite-preact/src/main.tsx
+++ examples/with-vite-preact/src/main.tsx
@@ -1,6 +1,5 @@
 import { render } from 'preact';
 import './index.css';
-import '@plumeria/core/stylesheet.css';
 import { App } from './app.tsx';
 const appElement = document.getElementById('app');
 if (!appElement) {
