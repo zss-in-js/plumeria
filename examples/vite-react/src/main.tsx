26ea98636e7176ca0eea8615a209689a15e99a2f
refirst11 <ocelot2828@gmail.com>
2026-01-10 21:48:52 +0900

    chore(vite-react): project name updated

--- examples/vite-react/src/main.tsx
+++ examples/vite-react/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import '@plumeria/core/stylesheet.css';
 import App from './App.tsx';
 
 const appElement = document.getElementById('root');
