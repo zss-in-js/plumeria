00d9b56597e82e2c8f1ebc0448bffd33fc56d580
refirst11 <ocelot2828@gmail.com>
2026-01-10 19:44:54 +0900

    chore(solid): remove plain solid

--- examples/with-solid/src/entry-client.tsx
+++
@@ -1,8 +0,0 @@
-// @refresh reload
-import { mount, StartClient } from '@solidjs/start/client';
-
-const appElement = document.getElementById('app');
-if (!appElement) {
-  throw new Error('App element not found');
-}
-mount(() => <StartClient />, appElement);
