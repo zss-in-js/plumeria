b90e7fcae6d8d6331cdba3f36f562b2e16d75b61
refirst11 <ocelot2828@gmail.com>
2026-01-10 19:55:41 +0900

    chore(vite-astro): updated

---
+++ examples/with-astro-react/src/components/style.ts
@@ -0,0 +1,11 @@
+import * as css from '@plumeria/core';
+
+const styles = css.create({
+  color: {
+    WebkitTextFillColor: 'transparent',
+    background: 'linear-gradient(90deg, #58c6ff 0%, #416389 50%, #ff3bef 100%)',
+    backgroundClip: 'text',
+  },
+});
+
+export const astroClass = css.props(styles.color);
