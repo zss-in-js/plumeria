4c7c1fa7836accf5c3264faf6e7857584457e0c6
refirst11 <ocelot2828@gmail.com>
2026-01-16 20:13:44 +0900

    feat(utils/optimizer.ts): add

---
+++ packages/utils/src/optimizer.ts
@@ -0,0 +1,23 @@
+import postcss from 'postcss';
+import combineMediaQuery from 'postcss-combine-media-query';
+import { transform as lightningCSSTransform } from 'lightningcss';
+
+export async function optimizer(cssCode: string): Promise<string> {
+  const merged = await postcss([combineMediaQuery()]).process(cssCode, {
+    from: undefined,
+  });
+
+  const light = lightningCSSTransform({
+    filename: 'global.css',
+    code: Buffer.from(merged.css),
+    minify: true,
+    targets: {
+      safari: 16,
+      edge: 110,
+      firefox: 110,
+      chrome: 110,
+    },
+  });
+
+  return Buffer.from(light.code).toString('utf-8');
+}
