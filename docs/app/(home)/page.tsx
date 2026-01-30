bff8c83db5110d24cb52e99f3b9855dc1c5fde51
refirst11 <ocelot2828@gmail.com>
2026-01-30 12:17:04 +0900

    chore(docs/app/(home)): update title and description

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -3,9 +3,9 @@ import { Metadata } from 'next';
 import generateSEOData from 'lib/generateSEOData';
 
 export const metadata: Metadata = generateSEOData({
-  title: 'Plumeria - Atomic CSS in JS library',
+  title: 'Plumeria - Type-only atomic CSS framework.',
   subtitle:
-    'The atomic on-demand CSS-in-JS. Plumeria compiles at build time, generating only the styles you use for optimal performance and size.',
+    'A type-only atomic CSS framework. Plumeria compiles at build time, generating only the styles you use for optimal performance and size.',
 });
 
 export default function Page() {
