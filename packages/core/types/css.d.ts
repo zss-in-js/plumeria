41c8e8528c6f9e6da2c623791d0a6b87d44d6845
refirst11 <ocelot2828@gmail.com>
2026-02-01 11:00:21 +0900

    chore(core/css.d.ts): improved marker and extended types

--- packages/core/types/css.d.ts
+++ packages/core/types/css.d.ts
@@ -8,22 +8,24 @@
  * type keyframes = (_rule: Keyframes) => string;
  * type viewTransition = (_rule: ViewTransition) => string;
  * type variants = <T extends Variants>(_rule: T) => (_props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
- * type marker = (_id: string, _pseudo: string) => CSSProperties;
- * type extended = (_id: string, _pseudo: string) => ContainerStyleQuery;
+ * type marker = (_id: string, _pseudo: string) => Marker;
+ * type extended = <I extends string, P extends string>(_id: I, _pseudo: P) => Extended<I, P>;
  * ```
  */
 declare module '@plumeria/core' {
-  export type CSSProperties = import('./definition').CSSProperties;
-  export type CreateStyle = import('./definition').CreateStyle;
-  export type CreateStyleType<T> = import('./definition').CreateStyleType<T>;
-  export type CreateStatic = import('./definition').CreateStatic;
-  export type CreateTheme = import('./definition').CreateTheme;
-  export type Keyframes = import('./definition').Keyframes;
-  export type ViewTransition = import('./definition').ViewTransition;
-  export type ReturnType<T> = import('./definition').ReturnType<T>;
-  export type ReturnVariableType<T> = import('./definition').ReturnVariableType<T>;
-  export type Variants = import('./definition').Variants;
-  export type ContainerStyleQuery = import('./definition').ContainerStyleQuery; 
+  export type CSSProperties = import('./definitions').CSSProperties;
+  export type CreateStyle = import('./definitions').CreateStyle;
+  export type CreateStyleType<T> = import('./definitions').CreateStyleType<T>;
+  export type CreateStatic = import('./definitions').CreateStatic;
+  export type CreateTheme = import('./definitions').CreateTheme;
+  export type Keyframes = import('./definitions').Keyframes;
+  export type ViewTransition = import('./definitions').ViewTransition;
+  export type ReturnType<T> = import('./definitions').ReturnType<T>;
+  export type ReturnVariableType<T> = import('./definitions').ReturnVariableType<T>;
+  export type Variants = import('./definitions').Variants;
+  export type Marker = import('./definitions').Marker;
+  export type Extended<I extends string, P extends string> = import('./definitions').Extended<I, P>;
+  export type ContainerStyleQuery = import('./definitions').ContainerStyleQuery; 
   export type create = <const T extends Record<string, CSSProperties>>(_rule: CreateStyleType<T>)=> ReturnType<T>;
   export type props = (..._rules: (false | CSSProperties | null | undefined)[])=> string;
   export type createTheme = <const T extends CreateTheme>(_rule: T)=> ReturnVariableType<T>;
@@ -31,8 +33,8 @@ declare module '@plumeria/core' {
   export type keyframes = (_rule: Keyframes) => string;
   export type viewTransition = (_rule: ViewTransition) => string;
   export type variants = <T extends Variants>(_rule: T) => (_props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
-  export type marker = (_id: string, _pseudo: string) => CSSProperties;
-  export type extended = (_id: string, _pseudo: string) => ContainerStyleQuery;
+  export type marker = (_id: string, _pseudo: string) => Marker;
+  export type extended = <I extends string, P extends string>(_id: I, _pseudo: P) => Extended<I, P>;
   export const create: create;
   export const props: props;
   export const createTheme: createTheme;
