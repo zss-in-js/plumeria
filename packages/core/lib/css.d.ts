edc99f158dcc8aa4c1a45e540e70e41bc36c27cb
refirst11 <ocelot2828@gmail.com>
2026-02-10 23:51:39 +0900

    chore(core/lib/css.d.ts): update

--- packages/core/lib/css.d.ts
+++ packages/core/lib/css.d.ts
@@ -2,15 +2,15 @@
  * Type definitions only. No runtime implementation provided.  
  * Configure the bundler plugin to extract and implement these APIs.
  * ```ts
- * type create = <const T extends Record<string, CSSProperties>>(_rule: CreateStyleType<T>)=> ReturnType<T>;
- * type props = (..._rules: (false | CSSProperties | null | undefined)[])=> string;
- * type createTheme = <const T extends CreateTheme>(_rule: T)=> ReturnVariableType<T>;
- * type createStatic = <const T extends CreateStatic>(_rule: T)=> T;
- * type keyframes = (_rule: Keyframes) => string;
- * type viewTransition = (_rule: ViewTransition) => string;
- * type variants = <T extends Variants>(_rule: T) => (_props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
- * type marker = (_id: string, _pseudo: string) => Marker;
- * type extended = <I extends string, P extends string>(_id: I, _pseudo: P) => Extended<I, P>;
+ * type create = <const T extends Record<string, CSSProperties>>(rule: CreateStyleType<T>)=> ReturnType<T>;
+ * type props = (...rules: (false | CSSProperties | null | undefined)[])=> string;
+ * type createTheme = <const T extends CreateTheme>(rule: T)=> ReturnVariableType<T>;
+ * type createStatic = <const T extends CreateStatic>(rule: T)=> T;
+ * type keyframes = (rule: Keyframes) => string;
+ * type viewTransition = (rule: ViewTransition) => string;
+ * type variants = <T extends Variants>(rule: T) => (props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
+ * type marker = (id: string, pseudo: string) => Marker;
+ * type extended = <I extends string, P extends string>(id: I, pseudo: P) => Extended<I, P>;
  * ```
  */
 declare module '@plumeria/core' {
@@ -27,26 +27,26 @@ declare module '@plumeria/core' {
     Extended,
   } from './types';
 
-  type CSSProperties = import('./types').CSSProperties;
-  type CreateStyle = import('./types').CreateStyle;
+  export type CSSProperties = import('./types').CSSProperties;
+  export type CreateStyle = import('./types').CreateStyle;
 
-  type create = <const T extends Record<string, CSSProperties>>(_rule: CreateStyleType<T>)=> ReturnType<T>;
-  type props = (..._rules: (false | CSSProperties | null | undefined)[])=> string;
-  type createTheme = <const T extends CreateTheme>(_rule: T)=> ReturnVariableType<T>;
-  type createStatic = <const T extends CreateStatic>(_rule: T)=> T;
-  type keyframes = (_rule: Keyframes) => string;
-  type viewTransition = (_rule: ViewTransition) => string;
-  type variants = <T extends Variants>(_rule: T) => (_props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
-  type marker = (_id: string, _pseudo: string) => Marker;
-  type extended = <I extends string, P extends string>(_id: I, _pseudo: P) => Extended<I, P>;
+  export type create = <const T extends Record<string, CSSProperties>>(rule: CreateStyleType<T>)=> ReturnType<T>;
+  export type props = (...rules: (false | CSSProperties | null | undefined)[])=> string;
+  export type createTheme = <const T extends CreateTheme>(rule: T)=> ReturnVariableType<T>;
+  export type createStatic = <const T extends CreateStatic>(rule: T)=> T;
+  export type keyframes = (rule: Keyframes) => string;
+  export type viewTransition = (rule: ViewTransition) => string;
+  export type variants = <T extends Variants>(rule: T) => (props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
+  export type marker = (id: string, pseudo: string) => Marker;
+  export type extended = <I extends string, P extends string>(id: I, pseudo: P) => Extended<I, P>;
 
-  const create: create;
-  const props: props;
-  const createTheme: createTheme;
-  const createStatic: createStatic;
-  const keyframes: keyframes;
-  const viewTransition: viewTransition;
-  const variants: variants;
-  const marker: marker;
-  const extended: extended;
+  export const create: create;
+  export const props: props;
+  export const createTheme: createTheme;
+  export const createStatic: createStatic;
+  export const keyframes: keyframes;
+  export const viewTransition: viewTransition;
+  export const variants: variants;
+  export const marker: marker;
+  export const extended: extended;
 }
