3e4179fd1f06383f541e89df999ba4be9472f3b8
refirst11 <ocelot2828@gmail.com>
2026-02-25 15:54:05 +0900

    chore(e2e-tests): removed

--- test-e2e/test/component-class-attach.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Components have correct classes applied', async ({ page }) => {
-  await page.goto('http://localhost:4000');
-  await page.waitForSelector('[data-testid="e2e-test-div"]');
-  const div = page.locator('[data-testid="e2e-test-div"]');
-  await expect(div).toHaveClass('x9khpzv9 xjh5z8ql');
-});
