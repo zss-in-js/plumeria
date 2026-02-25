b5663ab08e9351913cac67ee5afa813f664976fa
refirst11 <ocelot2828@gmail.com>
2026-02-25 16:38:35 +0900

    chore(e2e): add VariantTest component

---
+++ test-e2e/site/component/VariantTest.tsx
@@ -0,0 +1,54 @@
+import css from '@plumeria/core';
+
+const styles = css.create({
+  small: {
+    fontSize: '12px',
+  },
+  medium: {
+    fontSize: '16px',
+  },
+  large: {
+    fontSize: '20px',
+  },
+  primary: {
+    color: 'blue',
+  },
+  secondary: {
+    color: 'gray',
+  },
+});
+const variants = css.variants({
+  size: {
+    small: styles.small,
+    medium: styles.medium,
+    large: styles.large,
+  },
+  colorPick: {
+    primary: styles.primary,
+    secondary: styles.secondary,
+  },
+});
+
+export function VariantTest() {
+  return (
+    <div
+      style={{ marginTop: '20px', border: '1px solid #ccc', padding: '10px' }}
+    >
+      <h3>Variants Staticization Test</h3>
+      <div
+        data-testid="variant-div-1"
+        className={css.props(variants({ size: 'small', colorPick: 'primary' }))}
+      >
+        Small Primary Variant
+      </div>
+      <div
+        data-testid="variant-div-2"
+        className={css.props(
+          variants({ size: 'large', colorPick: 'secondary' }),
+        )}
+      >
+        Large Secondary Variant
+      </div>
+    </div>
+  );
+}
