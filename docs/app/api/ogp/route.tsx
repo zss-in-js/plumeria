778e91d225f419be23a26aae6cef1013b5bd7da7
refirst11 <ocelot2828@gmail.com>
2026-03-03 22:25:57 +0900

    chore(docs/app/api/ogp): update ogp card

--- docs/app/api/ogp/route.tsx
+++ docs/app/api/ogp/route.tsx
@@ -10,50 +10,48 @@ export async function GET(req: NextRequest) {
   const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : process.env.PROD_URL;
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
+        fontFamily: 'Geist Mono',
+        fontSize: 60,
+        fontWeight: 700,
+        color: '#fff',
+        backgroundImage: `url(${url + '/OGP_CARD.png'})`,
+        backgroundSize: '1200px 630px',
+      }}
+    >
       <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
           width: '100%',
           height: '100%',
-          fontFamily: 'Geist Mono',
-          fontSize: 60,
-          fontWeight: 700,
-          color: '#fff',
-          backgroundImage: `url(${url + '/BACKGROUND.png'})`,
-          backgroundSize: '1200px 630px',
+          padding: '200px',
+          fontSize: '30px',
+          color: 'rgb(188 188 188)',
+          textAlign: 'center',
         }}
       >
+        {title}
         <div
           style={{
+            position: 'absolute',
+            right: '40px',
+            bottom: '20px',
             display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            height: '100%',
-            padding: '200px',
-            fontSize: '30px',
-            color: 'rgb(188 188 188)',
-            textAlign: 'center',
+            fontSize: '24px',
           }}
         >
-          {title}
-          <div
-            style={{
-              position: 'absolute',
-              right: '40px',
-              bottom: '20px',
-              display: 'flex',
-              fontSize: '24px',
-            }}
-          >
-            {date}
-          </div>
+          {date}
         </div>
       </div>
-    ),
+    </div>,
   );
 }
