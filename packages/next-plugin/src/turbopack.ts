6bea98c987caff6750f0eaabe5673ddd4efa3af1
refirst11 <ocelot2828@gmail.com>
2026-03-04 20:58:27 +0900

    fix(next-plugin/turbopack.ts): the timing of the reset has been delegated to the moment the server is officially shut down

--- packages/next-plugin/src/turbopack.ts
+++ packages/next-plugin/src/turbopack.ts
@@ -2,8 +2,23 @@ import type {
   NextConfig,
   TurbopackLoaderItem,
 } from 'next/dist/server/config-shared';
+import fs from 'fs';
 
 export const withPlumeria = (nextConfig: NextConfig = {}): NextConfig => {
+  const VIRTUAL_FILE_PATH =
+    require.resolve('@plumeria/turbopack-loader/zero-virtual.css');
+  if (process.env.NODE_ENV === 'development') {
+    const cleanup = () => {
+      fs.writeFileSync(VIRTUAL_FILE_PATH, '/** Placeholder file */\n', 'utf-8');
+    };
+
+    process.on('SIGINT', cleanup);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    fs.writeFileSync(VIRTUAL_FILE_PATH, '/** Placeholder file */\n', 'utf-8');
+  }
+
   const reactLoaders: TurbopackLoaderItem[] = [
     {
       loader: '@plumeria/turbopack-loader',
