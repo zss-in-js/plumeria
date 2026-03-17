dd6e2bb5b026a7eb14365afb739f8885147fae94
refirst11 <ocelot2828@gmail.com>
2026-03-18 06:53:59 +0900

    chore(docs/layout.tsx): add header script

--- docs/app/(home)/layout.tsx
+++ docs/app/(home)/layout.tsx
@@ -1,7 +1,30 @@
-import type { ReactNode } from 'react';
+'use client';
+
+import { useEffect, type ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from 'app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const handleScroll = () => {
+      const nav = document.getElementById('nd-nav');
+      if (!nav) return;
+
+      if (window.scrollY > 0) {
+        nav.classList.add('is-scrolled');
+      } else {
+        nav.classList.remove('is-scrolled');
+      }
+    };
+
+    handleScroll();
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
 }
