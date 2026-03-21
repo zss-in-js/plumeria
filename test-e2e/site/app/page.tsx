325efc38dca0672d2387bb3138fc07c4cf1a3ff1
refirst11 <ocelot2828@gmail.com>
2026-03-21 21:14:00 +0900

    chore(test-e2e/site/app): update import

--- test-e2e/site/app/page.tsx
+++ test-e2e/site/app/page.tsx
@@ -2,9 +2,9 @@ import { ConditionalTest } from 'component/ConditionalTest';
 import { VariableTest } from 'component/VariableTest';
 import { VariantTest } from 'component/VariantTest';
 import Link from 'next/link';
-import css from '@plumeria/core';
+import style from '@plumeria/core';
 
-const styles = css.create({
+const styles = style.create({
   page: {
     color: 'orange',
   },
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <main>
       <h1>@plumeria/core E2E Test</h1>
-      <h2 className={css.props(styles.page)}>server component</h2>
+      <h2 className={style.use(styles.page)}>server component</h2>
       <Link href="/server">Server Page</Link>
       <ConditionalTest />
       <VariableTest />
