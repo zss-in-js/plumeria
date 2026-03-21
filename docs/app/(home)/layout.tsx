3836b398d751995b61a6c3677b692aabe78fa92c
refirst11 <ocelot2828@gmail.com>
2026-03-22 00:24:26 +0900

    chore(docs/home): layout.tsx remove script

--- docs/app/(home)/layout.tsx
+++ docs/app/(home)/layout.tsx
@@ -1,30 +1,6 @@
-'use client';
-
-import { useEffect, type ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from 'app/layout.config';
 
-export default function Layout({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    const handleScroll = () => {
-      const nav = document.getElementById('nd-nav');
-      if (!nav) return;
-
-      if (window.scrollY > 0) {
-        nav.classList.add('is-scrolled');
-      } else {
-        nav.classList.remove('is-scrolled');
-      }
-    };
-
-    handleScroll();
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
 }
