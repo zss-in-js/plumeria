4d5ce63261af4065d4b65d3243738374fcb92b59
refirst11 <ocelot2828@gmail.com>
2026-03-26 22:07:25 +0900

    test(eslint-plugin): add style-name-requires-import test passed

---
+++ packages/eslint-plugin/__tests__/rules/style-name-requires-import.test.ts
@@ -0,0 +1,89 @@
+import type { JSRuleDefinition } from 'eslint';
+import { RuleTester } from 'eslint';
+import { styleNameRequiresImport } from '../../src/rules/style-name-requires-import';
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
+});
+
+ruleTester.run(
+  'style-name-requires-import',
+  styleNameRequiresImport as JSRuleDefinition,
+  {
+    valid: [
+      {
+        code: `
+          import * as css from '@plumeria/core';
+          const el = <div styleName={[styles.foo]} />;
+        `,
+      },
+      {
+        code: `
+          import { create } from '@plumeria/core';
+          const el = <div styleName={[styles.foo]} />;
+        `,
+      },
+      {
+        code: `
+          import * as css from '@plumeria/core';
+          const el = <div className="foo" />;
+        `,
+      },
+      {
+        code: `
+          import React from 'react';
+          const el = <div className="foo" />;
+        `,
+      },
+      {
+        code: `
+          const el = <div className="foo" />;
+        `,
+      },
+    ],
+    invalid: [
+      {
+        code: `
+          const el = <div styleName={[styles.foo]} />;
+        `,
+        errors: [
+          {
+            messageId: 'styleNameError',
+          },
+        ],
+      },
+      {
+        code: `
+          import React from 'react';
+          const el = <div styleName={[styles.foo]} />;
+        `,
+        errors: [
+          {
+            messageId: 'styleNameError',
+          },
+        ],
+      },
+      {
+        code: `
+          import something from 'other-lib';
+          const el = <div styleName={[styles.foo]} />;
+          const el2 = <span styleName={[styles.bar]} />;
+        `,
+        errors: [
+          {
+            messageId: 'styleNameError',
+          },
+          {
+            messageId: 'styleNameError',
+          },
+        ],
+      },
+    ],
+  },
+);
