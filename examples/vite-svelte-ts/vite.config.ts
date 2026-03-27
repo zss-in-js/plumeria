eb3e09614d7c6a791f440e22e6be2ff1c434f184
refirst11 <ocelot2828@gmail.com>
2026-03-27 19:32:21 +0900

    chore(vite-svelte-ts): removed

--- examples/vite-svelte-ts/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { plumeria } from '@plumeria/vite-plugin';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [plumeria(), svelte()],
-});
