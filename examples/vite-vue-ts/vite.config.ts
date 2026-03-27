6684cb68b61d3e2e5f14fec0ac6dd11528e0e925
refirst11 <ocelot2828@gmail.com>
2026-03-27 19:32:04 +0900

    chore(vite-vue-ts): removed

--- examples/vite-vue-ts/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { plumeria } from '@plumeria/vite-plugin';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), plumeria()],
-});
