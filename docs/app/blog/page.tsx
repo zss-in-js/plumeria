879911dfcfdbfe8980b9def2a84964b9d8fff3eb
refirst11 <ocelot2828@gmail.com>
2026-03-27 16:47:28 +0900

    chore(docs/blog/page.tsx): update

--- docs/app/blog/page.tsx
+++ docs/app/blog/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import type { Metadata } from 'next/types';
 import { blog } from 'lib/source';
 import { styles } from './styles';
-import * as style from '@plumeria/core';
+import '@plumeria/core';
 import { JSX } from 'react';
 import generateSEOData from 'lib/generateSEOData';
 
@@ -15,20 +15,20 @@ function Page(): JSX.Element {
   const posts = blog.getPages();
 
   return (
-    <main className={style.use(styles.container)}>
-      <div className={style.use(styles.header)}>
-        <h1 className={style.use(styles.title)}>Blog</h1>
+    <main styleName={styles.container}>
+      <div styleName={styles.header}>
+        <h1 styleName={styles.title}>Blog</h1>
         <span>The latest updates and releases from the Plumeria at ZSS-in-JS.</span>
       </div>
 
       {posts
         .reverse()
         .map((post) => (
-          <Link href={`/blog/${post.slugs.join('/')}`} className={style.use(styles.card)} key={post.data.title}>
-            <h2 className={style.use(styles.cardTitle)}>{post.data.title}</h2>
-            <p className={style.use(styles.cardDesc)}>{post.data.description}</p>
-            <p className={style.use(styles.cardDesc)}>Read more →</p>
-            <p className={style.use(styles.cardDate)}>{post.data.date}</p>
+          <Link href={`/blog/${post.slugs.join('/')}`} styleName={styles.card} key={post.data.title}>
+            <h2 styleName={styles.cardTitle}>{post.data.title}</h2>
+            <p styleName={styles.cardDesc}>{post.data.description}</p>
+            <p styleName={styles.cardDesc}>Read more →</p>
+            <p styleName={styles.cardDate}>{post.data.date}</p>
           </Link>
         ))
         .sort()}
