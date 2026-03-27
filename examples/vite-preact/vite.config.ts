91b7555c239017f0d1fa9a77c794be760ce7f48d
refirst11 <ocelot2828@gmail.com>
2026-03-27 19:34:32 +0900

    chore(preact): removed

--- examples/vite-preact/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import { plumeria } from '@plumeria/vite-plugin';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [preact(), plumeria()],
-});
