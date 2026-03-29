c686aa210987394de9659cdaec4f7eb26f924128
refirst11 <ocelot2828@gmail.com>
2026-03-29 21:53:37 +0900

    chore(core/lib/css.d.ts): compatibility support for the use function

--- packages/core/lib/css.d.ts
+++ packages/core/lib/css.d.ts
@@ -10,6 +10,7 @@
  * type variants = <T extends Variants>(rule: T) => (props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
  * type marker = (id: string, pseudo: string) => Marker;
  * type extended = <I extends string, P extends string>(id: I, pseudo: P) => Extended<I, P>;
+ * type use = (...rules: (false | CSSProperties | null | undefined)[])=> string;
  * ```
  */
 declare module '@plumeria/core' {
@@ -30,7 +31,7 @@ declare module '@plumeria/core' {
   export type CSSProperties = import('./types').CSSProperties;
   export type CreateStyle = import('./types').CreateStyle;
 
-  export type create = <const T extends Record<string, CreateStyleValue>>(rule: CreateStyleType<T>)=> CreateReturnType<T>;
+  export type create = <const T extends Record<string, CreateStyleValue>>(rule: CreateStyleType<T>)=> CreateReturnType<T>; 
   export type createTheme = <const T extends CreateTheme>(rule: T)=> ReturnVariableType<T>;
   export type createStatic = <const T extends CreateStatic>(rule: T)=> T;
   export type keyframes = (rule: Keyframes) => string;
@@ -38,6 +39,7 @@ declare module '@plumeria/core' {
   export type variants = <T extends Variants>(rule: T) => (props: { [K in keyof T]?: keyof T[K] }) => CSSProperties;
   export type marker = (id: string, pseudo: string) => Marker;
   export type extended = <I extends string, P extends string>(id: I, pseudo: P) => Extended<I, P>;
+  export type use = (...rules: (false | CSSProperties | null | undefined)[])=> string;
 
   export const create: create; 
   export const createTheme: createTheme;
@@ -47,6 +49,7 @@ declare module '@plumeria/core' {
   export const variants: variants;
   export const marker: marker;
   export const extended: extended;
+  export const use: use;
 
   declare global {
       namespace React {
