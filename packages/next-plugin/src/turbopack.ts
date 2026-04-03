dbc4cfb59722166c83dc151a2d501ac771ecc76e
refirst11 <ocelot2828@gmail.com>
2026-04-03 18:59:50 +0900

    chore(next-plugin/turbopack.ts): remove unnecessary init process

--- packages/next-plugin/src/turbopack.ts
+++ packages/next-plugin/src/turbopack.ts
@@ -2,18 +2,8 @@ import type {
   NextConfig,
   TurbopackLoaderItem,
 } from 'next/dist/server/config-shared';
-import fs from 'fs';
-
-const PLACEHOLDER = '/** Placeholder file */\n';
 
 export const withPlumeria = (nextConfig: NextConfig = {}): NextConfig => {
-  const VIRTUAL_FILE_PATH =
-    require.resolve('@plumeria/turbopack-loader/zero-virtual.css');
-
-  if (process.env.NODE_ENV === 'production') {
-    fs.writeFileSync(VIRTUAL_FILE_PATH, PLACEHOLDER, 'utf-8');
-  }
-
   const reactLoaders: TurbopackLoaderItem[] = [
     { loader: '@plumeria/turbopack-loader', options: {} },
   ];
