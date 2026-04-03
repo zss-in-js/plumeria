0cac86f01fc30a884a4cb6d0a345022ac3c49e96
refirst11 <ocelot2828@gmail.com>
2026-04-03 11:31:33 +0900

    chore(docs/component): update CodeBlock.tsx

--- docs/component/CodeBlock.tsx
+++ docs/component/CodeBlock.tsx
@@ -1,26 +1,25 @@
 import { codeToHtml } from 'shiki';
 import * as css from '@plumeria/core';
 import { breakpoints } from 'lib/mediaQuery';
+import { CodeBlock as FumadocsCodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 
-const stylesCode = css.create({
+const styles = css.create({
   code_div: {
     position: 'relative',
     zIndex: 0,
     display: 'flex',
     width: '100%',
     maxWidth: '590px',
-    padding: '16px 0px',
     overflow: 'hidden',
-    fontFamily: 'var(--font-geist-mono)',
-    fontSize: 11,
-    border: '1px solid var(--card-border)',
-    borderRadius: '12px',
-    backdropFilter: 'blur(12px)',
     [breakpoints.md]: {
       maxWidth: '340px',
       overflowX: 'scroll',
       textAlign: 'left',
     },
+  },
+  pre: {
+    fontFamily: 'var(--font-geist-mono)',
+    fontSize: 11,
   },
 });
 
@@ -34,8 +33,8 @@ export const CodeBlock = async ({ code, lang }: { code: string; lang: string }) 
     defaultColor: false,
   });
   return (
-    <figure className={css.props(stylesCode.code_div)}>
-      <pre dangerouslySetInnerHTML={{ __html: tsx }} />
-    </figure>
+    <FumadocsCodeBlock styleName={styles.code_div}>
+      <Pre styleName={styles.pre} dangerouslySetInnerHTML={{ __html: tsx }} />
+    </FumadocsCodeBlock>
   );
 };
