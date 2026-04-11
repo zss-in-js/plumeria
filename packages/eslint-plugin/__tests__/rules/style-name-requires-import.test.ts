2f3c1f58430dbeef20aa43fe38f91631be049426
refirst11 <ocelot2828@gmail.com>
2026-04-11 21:45:22 +0900

    chore(eslint-plugin/__tests__): remove type assertion in style-name-requires-import.test

--- packages/eslint-plugin/__tests__/rules/style-name-requires-import.test.ts
+++ packages/eslint-plugin/__tests__/rules/style-name-requires-import.test.ts
@@ -1,4 +1,3 @@
-import type { JSRuleDefinition } from 'eslint';
 import { RuleTester } from 'eslint';
 import { styleNameRequiresImport } from '../../src/rules/style-name-requires-import';
 
@@ -12,78 +11,74 @@ const ruleTester = new RuleTester({
   },
 });
 
-ruleTester.run(
-  'style-name-requires-import',
-  styleNameRequiresImport as JSRuleDefinition,
-  {
-    valid: [
-      {
-        code: `
+ruleTester.run('style-name-requires-import', styleNameRequiresImport, {
+  valid: [
+    {
+      code: `
           import * as css from '@plumeria/core';
           const el = <div styleName={[styles.foo]} />;
         `,
-      },
-      {
-        code: `
+    },
+    {
+      code: `
           import { create } from '@plumeria/core';
           const el = <div styleName={[styles.foo]} />;
         `,
-      },
-      {
-        code: `
+    },
+    {
+      code: `
           import * as css from '@plumeria/core';
           const el = <div className="foo" />;
         `,
-      },
-      {
-        code: `
+    },
+    {
+      code: `
           import React from 'react';
           const el = <div className="foo" />;
         `,
-      },
-      {
-        code: `
+    },
+    {
+      code: `
           const el = <div className="foo" />;
         `,
-      },
-    ],
-    invalid: [
-      {
-        code: `
+    },
+  ],
+  invalid: [
+    {
+      code: `
           const el = <div styleName={[styles.foo]} />;
         `,
-        errors: [
-          {
-            messageId: 'styleNameError',
-          },
-        ],
-      },
-      {
-        code: `
+      errors: [
+        {
+          messageId: 'styleNameError',
+        },
+      ],
+    },
+    {
+      code: `
           import React from 'react';
           const el = <div styleName={[styles.foo]} />;
         `,
-        errors: [
-          {
-            messageId: 'styleNameError',
-          },
-        ],
-      },
-      {
-        code: `
+      errors: [
+        {
+          messageId: 'styleNameError',
+        },
+      ],
+    },
+    {
+      code: `
           import something from 'other-lib';
           const el = <div styleName={[styles.foo]} />;
           const el2 = <span styleName={[styles.bar]} />;
         `,
-        errors: [
-          {
-            messageId: 'styleNameError',
-          },
-          {
-            messageId: 'styleNameError',
-          },
-        ],
-      },
-    ],
-  },
-);
+      errors: [
+        {
+          messageId: 'styleNameError',
+        },
+        {
+          messageId: 'styleNameError',
+        },
+      ],
+    },
+  ],
+});
