f48d13031e3f991f755eccc6e92b770bb50cf645
refirst11 <ocelot2828@gmail.com>
2026-04-17 18:15:55 +0900

    chore(examples): add tanstack-start

---
+++ examples/tanstack-start/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import rsc from '@vitejs/plugin-rsc';
+import { plumeria } from '@plumeria/vite-plugin';
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart({
+      rsc: {
+        enabled: true,
+      },
+    }),
+    viteReact(),
+    rsc(),
+    plumeria(),
+  ],
+});
+
+export default config;
