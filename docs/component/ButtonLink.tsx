d212c572be70b91b538ba560a1b35aa879d0524b
refirst11 <ocelot2828@gmail.com>
2026-04-29 16:38:32 +0900

    chore(docs/component): update ButtonLink.tsx

--- docs/component/ButtonLink.tsx
+++ docs/component/ButtonLink.tsx
@@ -2,6 +2,7 @@ import * as css from '@plumeria/core';
 import type { ReactNode } from 'react';
 import { breakpoints } from 'lib/mediaQuery';
 import Link from 'next/link';
+import { pseudos } from 'lib/pseudos';
 
 const styles = css.create({
   button: {
@@ -20,30 +21,44 @@ const styles = css.create({
     border: 'none',
     borderRadius: 'var(--border-radius)',
     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    transition: 'all 0.16s ease-in-out',
+    transitionDuration: '0.2s',
     '[disabled]': {
       pointerEvents: 'none',
       cursor: 'not-allowed',
       opacity: 0.6,
     },
-    ':hover': {
-      boxShadow: '0 7px 10px rgba(0, 0, 0, 0.15)',
-      filter: 'brightness(0.95)',
-    },
     ':active': {
-      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-      filter: 'brightness(0.9)',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.02)',
     },
     ':focus': {
       outline: 'none',
     },
   },
   getstarted: {
-    background: 'var(--plume-accent)',
+    color: 'var(--text-main-header-line)',
+    background: 'var(--card-bg)',
+    borderRadius: '16px',
+    boxShadow: 'var(--card-box-shadow)',
+    transition: 'all 0.3s ease',
+    [pseudos.hover]: {
+      scale: 1.1,
+    },
+    [breakpoints.md]: {
+      padding: '12px 24px',
+    },
   },
   installation: {
-    color: '#333536',
-    background: 'rgb(230, 230, 230)',
+    color: 'var(--text-main-header-line)',
+    background: 'var(--card-bg)',
+    borderRadius: '16px',
+    boxShadow: 'var(--card-box-shadow)',
+    transition: 'all 0.3s ease',
+    [pseudos.hover]: {
+      scale: 1.05,
+    },
+    [breakpoints.md]: {
+      padding: '12px 24px',
+    },
   },
   medium: {
     '--padding': '12px 32px',
