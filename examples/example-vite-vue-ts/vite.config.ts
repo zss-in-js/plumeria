fddcbccbc5e936efc79effaad9118f5e243b50a2
refirst11 <ocelot2828@gmail.com>
2026-05-03 21:42:51 +0900

    chore(examples): rename  example-vite-vue-ts

--- examples/example-vite-vue-ts/vite.config.ts
+++ examples/example-vite-vue-ts/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { plumeria } from '@plumeria/vite-plugin';
+import plumeria from '@plumeria/unplugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), plumeria()],
+  plugins: [vue(), plumeria.vite()],
 });
