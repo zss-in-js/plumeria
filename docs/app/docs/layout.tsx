279cc0a7b89bddc7db85247b60fd65d76ee964d7
refirst11 <ocelot2828@gmail.com>
2026-05-04 11:53:20 +0900

    chore(docs/app/docs): update layout.tsx

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,9 +1,11 @@
+'use cache';
+
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from 'app/layout.config';
 import { source } from 'lib/source';
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout sidebar={{ collapsible: false }} tree={source.pageTree} {...baseOptions}>
       {children}
