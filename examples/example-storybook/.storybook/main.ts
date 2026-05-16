f2d359c444275825e9942611d417e21ca168780d
refirst11 <ocelot2828@gmail.com>
2026-05-16 19:18:00 +0900

    chore(examples): add example-storybook

---
+++ examples/example-storybook/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
