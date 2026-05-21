1b09a9a7bb4d6087c8df455cf999375db0618065
refirst11 <ocelot2828@gmail.com>
2026-05-21 22:10:36 +0900

    chore(docs/app/home): update metadata in page.tsx

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import generateSEOData from 'lib/generateSEOData';
 
 export const metadata: Metadata = generateSEOData({
-  title: 'Plumeria - Abstraction layer',
+  title: 'Plumeria - Zero-cost abstraction layer',
   subtitle: 'Plumeria is a zero-cost abstraction layer for styling React components.',
 });
 
