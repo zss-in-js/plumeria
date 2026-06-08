9bef9db8b8baabdfde1fac1919f9cded22dae0d6
refirst11 <ocelot2828@gmail.com>
2026-06-08 16:45:07 +0900

    chore(docs/app/docs): update layout.tsx

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,12 +1,10 @@
-'use cache';
-
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import * as React from 'react';
 import type { ReactNode } from 'react';
 import { baseOptions } from 'app/layout.config';
 import { source } from 'lib/source';
 
-export default async function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   const targets = ['Documentation', 'API', 'Integrations', 'Blog'];
   const docsOptions = {
     ...baseOptions,
