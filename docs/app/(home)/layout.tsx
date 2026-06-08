352385219e356651dc5275446cff0e9b889d041b
refirst11 <ocelot2828@gmail.com>
2026-06-08 16:43:51 +0900

    chore(docs/app/home): update layout.tsx

--- docs/app/(home)/layout.tsx
+++ docs/app/(home)/layout.tsx
@@ -1,9 +1,7 @@
-'use cache';
-
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from 'app/layout.config';
 
-export default async function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
 }
