ddd1f2942bd50d4ca5c41ee70b4312a481a1bb65
refirst11 <ocelot2828@gmail.com>
2026-06-26 19:40:25 +0900

    chore(utils/types.ts): update createTheme types

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -49,8 +49,8 @@ export interface Tables {
 
 export type CreateTheme = {
   [key: string]: {
-    default: string | number;
-    theme: string | number;
+    default: string;
+    theme: string;
   };
 };
 
