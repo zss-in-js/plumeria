d83abac0c0494b0007bd2890d229070e9d6e31bb
refirst11 <ocelot2828@gmail.com>
2026-06-30 20:26:04 +0900

    chore(headlessui): refactor export in index.ts

--- packages/headlessui/src/index.ts
+++ packages/headlessui/src/index.ts
@@ -1,6 +1,40 @@
-export * from './Dialog';
-export * from './Popover';
-export * from './Tooltip';
-export * from './Accordion';
-export * from './Tabs';
-export * from './Collapsible';
+export {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from './Dialog';
+export {
+  Popover,
+  PopoverTrigger,
+  PopoverPortal,
+  PopoverContent,
+  PopoverClose,
+  PopoverArrow,
+  PopoverAnchor,
+} from './Popover';
+export {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipPortal,
+  TooltipContent,
+  TooltipArrow,
+} from './Tooltip';
+export {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionTrigger,
+  AccordionContent,
+} from './Accordion';
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './Tabs';
+export {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from './Collapsible';
