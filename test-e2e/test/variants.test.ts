8bded1dd6b6b23a8eea9a61d06d437422dac70c6
refirst11 <ocelot2828@gmail.com>
2026-07-11 18:52:45 +0900

    feat(test-e2e/test): update variants.test.ts

--- test-e2e/test/variants.test.ts
+++ test-e2e/test/variants.test.ts
@@ -11,4 +11,13 @@ test('Variants are correctly staticized and applied', async ({ page }) => {
   const div2 = page.locator('[data-testid="variant-div-2"]');
   await expect(div2).toHaveCSS('font-size', '20px');
   await expect(div2).toHaveCSS('color', 'rgb(128, 128, 128)'); // gray
+
+  const bdiv1 = page.locator('[data-testid="bracket-div-1"]');
+  await expect(bdiv1).toHaveCSS('color', 'rgb(0, 128, 0)'); // green
+
+  const bdiv2 = page.locator('[data-testid="bracket-div-2"]');
+  await expect(bdiv2).toHaveCSS('color', 'rgb(128, 0, 128)'); // purple
+
+  const bdiv3 = page.locator('[data-testid="bracket-div-3"]');
+  await expect(bdiv3).toHaveCSS('color', 'rgb(0, 128, 0)'); // green
 });
