50043c69685b914a891d4662255c47a52a76b01c
refirst11 <ocelot2828@gmail.com>
2026-07-20 10:33:36 +0900

    chore(docs/app): disallow api root in robots.ts

--- docs/app/robots.ts
+++ docs/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/api/',
     },
     sitemap: process.env.PROD_URL + '/sitemap.xml',
   };
