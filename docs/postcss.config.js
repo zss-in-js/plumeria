ac7724734231c1e1f3d0f9471a859c909574af17
refirst11 <ocelot2828@gmail.com>
2026-07-28 11:21:23 +0900

    chore(docs): remove postcss.config.js

--- docs/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    '@plumeria/postcss-plugin': {
-      include: ['./app/**/*.{ts,tsx}', './component/**/*.{ts,tsx}', './lib/**/*.{ts,tsx}'],
-      exclude: ['**/node_modules/**', '**/.next/**'],
-    },
-  },
-};
