3bb4d1c7331730c4d8459bd882e9ab2d0d5d8298
refirst11 <ocelot2828@gmail.com>
2026-07-30 15:30:58 +0900

    chore(examples/example-vite-react): rename styleName to classStyle in App.tsx

--- examples/example-vite-react/src/App.tsx
+++ examples/example-vite-react/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 styleName={styles.color}>Vite + React</h1>
+      <h1 classStyle={styles.color}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
