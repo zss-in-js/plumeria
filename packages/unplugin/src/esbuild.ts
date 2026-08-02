76bf579dd0bc23ab723bbcd280c0636726a595e6
refirst11 <ocelot2828@gmail.com>
2026-08-02 23:21:56 +0900

    fix(unplugin): strip the virtual css annotation from the output in esbuild.ts

--- packages/unplugin/src/esbuild.ts
+++ packages/unplugin/src/esbuild.ts
@@ -1,4 +1,32 @@
 import { createEsbuildPlugin } from 'unplugin';
+import * as path from 'path';
 import { unpluginFactory, type PluginOptions } from './core';
+import { stripCssMarkersIn } from './css-marker';
 
-export default createEsbuildPlugin<PluginOptions | undefined>(unpluginFactory);
+type EsbuildOutputOptions = { outdir?: string; outfile?: string };
+
+function attachEsbuildHooks(plugin: any) {
+  const outputs = new Set<string>();
+
+  return {
+    ...plugin,
+
+    esbuild: {
+      ...plugin.esbuild,
+      config(this: unknown, options: EsbuildOutputOptions) {
+        plugin.esbuild?.config?.call(this, options);
+        if (options.outdir) outputs.add(options.outdir);
+        if (options.outfile) outputs.add(path.dirname(options.outfile));
+      },
+    },
+
+    writeBundle() {
+      stripCssMarkersIn(outputs);
+    },
+  };
+}
+
+export default createEsbuildPlugin<PluginOptions | undefined>(
+  (options, metaOptions) =>
+    attachEsbuildHooks(unpluginFactory(options, metaOptions)),
+);
