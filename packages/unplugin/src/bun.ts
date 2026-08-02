f4a1d4fdf44586794de9b914b51330089b0313b5
refirst11 <ocelot2828@gmail.com>
2026-08-02 23:21:57 +0900

    fix(unplugin): strip the virtual css annotation from the output in bun.ts

--- packages/unplugin/src/bun.ts
+++ packages/unplugin/src/bun.ts
@@ -1,4 +1,26 @@
 import { createBunPlugin } from 'unplugin';
 import { unpluginFactory, type PluginOptions } from './core';
+import { stripCssMarkersIn } from './css-marker';
 
-export default createBunPlugin<PluginOptions | undefined>(unpluginFactory);
+const createPlugin = (outputs: Set<string>) =>
+  createBunPlugin<PluginOptions | undefined>((options, metaOptions) => ({
+    ...unpluginFactory(options, metaOptions),
+    writeBundle() {
+      stripCssMarkersIn(outputs);
+    },
+  }));
+
+export default (options?: PluginOptions) => {
+  const outputs = new Set<string>();
+  const plugin = createPlugin(outputs)(options) as any;
+  const setup = plugin.setup;
+
+  return {
+    ...plugin,
+    async setup(build: any) {
+      const outdir = build?.config?.outdir;
+      if (outdir) outputs.add(outdir);
+      return setup.call(plugin, build);
+    },
+  };
+};
