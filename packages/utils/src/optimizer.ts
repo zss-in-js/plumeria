88cff0f1f16cb59d0d6620a3edc381d7d05dc004
refirst11 <ocelot2828@gmail.com>
2026-08-12 19:19:39 +0900

    fix(utils): use mergeRules in optimizer

--- packages/utils/src/optimizer.ts
+++ packages/utils/src/optimizer.ts
@@ -1,50 +1,9 @@
 import postcss from 'postcss';
-import combineMediaQuery from 'postcss-combine-media-query';
 import { transform as lightningCSSTransform } from 'lightningcss';
-
-function isMediaRule(rule: string): boolean {
-  let i = 0;
-
-  while (i < rule.length) {
-    const char = rule[i];
-
-    if (char === '/' && rule[i + 1] === '*') {
-      const end = rule.indexOf('*/', i + 2);
-      if (end === -1) return false;
-      i = end + 2;
-      continue;
-    }
-
-    if (
-      char === ' ' ||
-      char === '\n' ||
-      char === '\t' ||
-      char === '\r' ||
-      char === '\f'
-    ) {
-      i++;
-      continue;
-    }
-
-    break;
-  }
-
-  return rule.startsWith('@media', i);
-}
-
-export function orderMediaLast(rules: string[]): string[] {
-  const base: string[] = [];
-  const media: string[] = [];
-
-  for (const rule of rules) {
-    (isMediaRule(rule) ? media : base).push(rule);
-  }
-
-  return [...base, ...media];
-}
+import { mergeRules } from './mergeRules';
 
 export async function optimizer(cssCode: string): Promise<string> {
-  const merged = await postcss([combineMediaQuery()]).process(cssCode, {
+  const merged = await postcss([mergeRules()]).process(cssCode, {
     from: undefined,
   });
 
