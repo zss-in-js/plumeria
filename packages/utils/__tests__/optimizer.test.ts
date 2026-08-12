e1a0e2a27f6c73515f271aa6b544f679459358a0
refirst11 <ocelot2828@gmail.com>
2026-08-12 19:18:43 +0900

    chore(utils/__tests__): update optimizer.test.ts

--- packages/utils/__tests__/optimizer.test.ts
+++ packages/utils/__tests__/optimizer.test.ts
@@ -1,43 +1,16 @@
-import { orderMediaLast } from '../src/optimizer';
+import { optimizer } from '../src/optimizer';
 
-describe('orderMediaLast', () => {
-  test('moves media rules after the base rules', () => {
-    expect(
-      orderMediaLast([
-        '@media (min-width: 640px){.a{color:red}}',
-        '.b{color:b}',
-      ]),
-    ).toEqual(['.b{color:b}', '@media (min-width: 640px){.a{color:red}}']);
-  });
+describe('optimizer', () => {
+  it('merges identical selectors and at-rules', async () => {
+    const result = await optimizer(
+      '@container (min-width:400px){.a{color:red}}' +
+        '.base{color:black}' +
+        '@container (min-width:400px){.a{display:block}.b{color:blue}}',
+    );
 
-  test('detects a media rule behind leading whitespace', () => {
-    const media = ' \n\t\r\f@media (min-width: 640px){.a{color:red}}';
-    expect(orderMediaLast([media, '.b{color:b}'])).toEqual([
-      '.b{color:b}',
-      media,
-    ]);
-  });
-
-  test('detects a media rule behind a leading comment', () => {
-    const media = '/* start */@media screen{.a{color:red}}';
-    expect(orderMediaLast([media, '.b{color:b}'])).toEqual([
-      '.b{color:b}',
-      media,
-    ]);
-  });
-
-  test('treats an unterminated comment as a base rule', () => {
-    const rule = '/* never closed @media screen{.a{color:red}}';
-    expect(orderMediaLast([rule, '.b{color:b}'])).toEqual([
-      rule,
-      '.b{color:b}',
-    ]);
-  });
-
-  test('treats a rule that only has whitespace as a base rule', () => {
-    expect(orderMediaLast(['   ', '.b{color:b}'])).toEqual([
-      '   ',
-      '.b{color:b}',
-    ]);
+    expect(result.match(/@container/g)).toHaveLength(1);
+    expect(result.match(/\.a\s*\{/g)).toHaveLength(1);
+    expect(result).toContain('color: red');
+    expect(result).toContain('display: block');
   });
 });
