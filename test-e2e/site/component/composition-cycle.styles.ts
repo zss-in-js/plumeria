c1c8a4dff1c1e05356caea0723da1bdfed544665
refirst11 <ocelot2828@gmail.com>
2026-08-18 17:19:14 +0900

    chore(test-e2e/site/component): add composition-cycle.styles.ts

---
+++ test-e2e/site/component/composition-cycle.styles.ts
@@ -0,0 +1,14 @@
+import * as css from '@plumeria/core';
+
+export const cycle = css.create({
+  surface: {
+    padding: 8,
+    color: 'rgb(0, 0, 0)',
+    background: 'rgb(238, 238, 238)',
+  },
+  raised: {
+    padding: 16,
+    color: 'rgb(255, 255, 255)',
+    background: 'rgb(51, 51, 51)',
+  },
+});
