3cf7c8572c5431126aa4e713b9c24d43488a68f7
refirst11 <ocelot2828@gmail.com>
2026-08-18 17:19:13 +0900

    chore(test-e2e/site/component): add composition-branch.styles.ts

---
+++ test-e2e/site/component/composition-branch.styles.ts
@@ -0,0 +1,13 @@
+import * as css from '@plumeria/core';
+
+export const branch = css.create({
+  neutral: {
+    color: 'rgb(128, 128, 128)',
+  },
+  danger: {
+    color: 'rgb(220, 20, 60)',
+  },
+  safe: {
+    color: 'rgb(46, 139, 87)',
+  },
+});
