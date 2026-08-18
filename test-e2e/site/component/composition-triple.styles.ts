ad42fa422e0b197d77b17b47b65f8b2cc898e85e
refirst11 <ocelot2828@gmail.com>
2026-08-18 17:19:15 +0900

    chore(test-e2e/site/component): add composition-triple.styles.ts

---
+++ test-e2e/site/component/composition-triple.styles.ts
@@ -0,0 +1,16 @@
+import * as css from '@plumeria/core';
+
+export const triple = css.create({
+  one: {
+    padding: 1,
+    color: 'rgb(255, 0, 0)',
+  },
+  two: {
+    padding: 2,
+    color: 'rgb(0, 0, 255)',
+  },
+  three: {
+    padding: 3,
+    color: 'rgb(0, 128, 0)',
+  },
+});
