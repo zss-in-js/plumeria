a30bd10d68390192e262ea7a0626d427b49de94a
refirst11 <ocelot2828@gmail.com>
2026-08-18 17:04:08 +0900

    chore(test-e2e/site/app): update page.tsx

--- test-e2e/site/app/page.tsx
+++ test-e2e/site/app/page.tsx
@@ -1,4 +1,5 @@
 import { CascadeTest } from 'component/CascadeTest';
+import { CompositionOrderTest } from 'component/CompositionOrderTest';
 import { ConditionalTest } from 'component/ConditionalTest';
 import { ContainerQueryTest } from 'component/ContainerQueryTest';
 import { QueryOrderTest } from 'component/QueryOrderTest';
@@ -21,6 +22,7 @@ export default function Home() {
       <h2 classStyle={styles.page}>server component</h2>
       <Link href="/server">Server Page</Link>
       <CascadeTest />
+      <CompositionOrderTest />
       <ContainerQueryTest />
       <QueryOrderTest />
       <ConditionalTest />
