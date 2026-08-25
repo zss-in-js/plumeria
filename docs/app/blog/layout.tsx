64529cf0cf966a01a70f21a69035939e0927dda6
refirst11 <ocelot2828@gmail.com>
2026-08-25 20:55:31 +0900

    chore(docs/app/blog): load the katex stylesheet in layout.tsx

--- docs/app/blog/layout.tsx
+++ docs/app/blog/layout.tsx
@@ -1,4 +1,5 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
+import 'katex/dist/katex.css';
 import type { ReactNode } from 'react';
 import { baseOptions } from 'app/layout.config';
 import { source } from 'lib/source';
